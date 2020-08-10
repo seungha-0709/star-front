917e08707ba19b092795629af607cef1bc7f60f8
dongmin <ehdals0227@gmail.com>
2020-08-10 21:19:41 +0900

    Add: storybook 설정 / 테스트 컴포넌트 추가

---
+++ components/List.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="list">
+		I'm a list
+	</div>
+</template>
+<script>
+export default {
+	name: "List",
+};
+</script>
+<style scoped>
+.list {
+	background: #ccc;
+}
+</style>
---
+++ stories/list.stories.js
@@ -0,0 +1,9 @@
+import { storiesOf } from "@storybook/vue";
+import List from "../components/List.vue";
+
+storiesOf("List", module)
+	.add("As a component", () => ({
+		components: { List },
+		template: "<List />",
+	}))
+	.add("I don't work", () => "<List />");
