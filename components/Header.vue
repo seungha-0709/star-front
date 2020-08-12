2256052731c9fa0d2da945e517a7cea9a7d0cdce
dongmin <ehdals0227@gmail.com>
2020-08-12 19:01:07 +0900

    Add: header 컴포넌트 추가

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>헤더</header>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  header {
+    height: 80px;
+    background-color: teal;
+  }
+</style>
