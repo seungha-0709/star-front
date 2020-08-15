9545f48e952e2ae585cf8dd531ab2cbed996892c
dongmin <ehdals0227@gmail.com>
2020-08-15 17:44:21 +0900

    Add: scrollTop 컴포넌트 추가 / 적용

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       컨텐츠 내용
+      <div class="scroll-test">스크롤 영역</div>
     </v-flex>
   </v-layout>
 </template>
+
+<style scoped>
+.scroll-test {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 800px;
+}
+</style>
