539e46ae170dfde16afe91dc2a598598b7fa76e0
dongmin <ehdals0227@gmail.com>
2020-08-15 17:41:22 +0900

    Add: 스토리북 + vutify 옵션 설정 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,19 @@
-import { configure } from "@storybook/vue"
+import { configure, addDecorator } from "@storybook/vue"
+
+import "vuetify/dist/vuetify.css"
+
+import Vue from "vue"
+import Vuetify from "vuetify"
+
+Vue.use(Vuetify)
+
 function loadStories() {
   const req = require.context("../stories", true, /\.stories\.js$/)
   req.keys().forEach((filename) => req(filename))
 }
+
+addDecorator(() => ({
+  template: "<v-app><story/></v-app>"
+}))
+
 configure(loadStories, module)
