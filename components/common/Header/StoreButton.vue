a9f5abc9411c5ad7fc780cce91aba0e6f0cf90f3
dongmin <ehdals0227@gmail.com>
2020-08-16 18:29:47 +0900

    Add: 스토어 이동 버튼 컴포넌트 추가

---
+++ components/common/Header/StoreButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" class="btn-store" @click="$router.push('store')">
+    <img src alt="심볼 로고" />
+    별별스토어
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.btn-store {
+  width: 157px;
+  height: 42px;
+  line-height: 42px;
+  border-radius: 23px;
+  background-image: linear-gradient(255deg, #5e3fcc, #3f60cc);
+  font-size: 18px;
+  color: #fff;
+}
+</style>
