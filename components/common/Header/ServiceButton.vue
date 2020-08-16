f9df05c8d507cba5787ad40e7fb2c576ca8cc8ff
dongmin <ehdals0227@gmail.com>
2020-08-16 19:06:07 +0900

    Add: header - 서비스 버튼 컴포넌트 추가

---
+++ components/common/Header/ServiceButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button type="button" class="btn-service" v-bind:style="{ marginRight: marginRight + 'px' }">
+    <v-icon size="medium">{{icon}}</v-icon>
+  </button>
+</template>
+<script>
+export default {
+  props: ["icon", "margin-right"],
+  data() {
+    return {
+      icon: this.icon,
+      marginRight: this.marginRight
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn-service {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid #dfdfdf;
+  vertical-align: top;
+}
+</style>
