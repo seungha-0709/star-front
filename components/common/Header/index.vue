71219779b178166a7aef4fffaca14f46df5abe47
dongmin <ehdals0227@gmail.com>
2020-08-16 18:30:45 +0900

    Add: header폴더 생성 / 관련 컴포넌트 추가,적용

---
+++ components/common/Header/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <div class="header-wrap">
+      <div class="logo">
+        <img src alt="로고" />
+      </div>
+      <global-nav-bar></global-nav-bar>
+      <btn-link-store></btn-link-store>
+    </div>
+  </header>
+</template>
+
+<script>
+import gnbVue from "./gnb.vue"
+import StoreButtonVue from "./StoreButton.vue"
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    "global-nav-bar": gnbVue,
+    "btn-link-store": StoreButtonVue
+  }
+}
+</script>
+
+<style scoped>
+header {
+  height: 68px;
+  background-color: #fff;
+  border-bottom: 1px solid #dfdfdf;
+}
+.header-wrap {
+  width: 1240px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+}
+</style>
