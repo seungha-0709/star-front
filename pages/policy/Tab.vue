de0a8052830d094cbb5993dfa6d1554d5b29d4bc
Seungha <sh.kim.x928@gmail.com>
2020-08-21 12:38:11 +0900

    Add: policy 3 pages & link

---
+++ pages/policy/Tab.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-content">
+    <div v-if="tabType === 'service'">
+      <service />
+    </div>
+    <div v-else-if="tabType === 'privacy'">
+      <privacy />
+    </div>
+    <div v-else-if="tabType === 'marketing'">
+      <marketing />
+    </div>
+    <div v-else-if="tabType === 'email'">
+      <email />
+    </div>
+    <div v-else-if="tabType === 'ranking_logic'">
+      <rankingLogic />
+    </div>
+  </div>
+</template>
+
+<script>
+import service from './service'
+import privacy from './privacy'
+import marketing from './marketing'
+import email from './email'
+import rankingLogic from './rankingLogic'
+
+export default {
+  props: ["tab-type"],
+  components: {
+    service,
+    privacy,
+    marketing,
+    email,
+    rankingLogic
+  }
+}
+</script>
+
+<style scoped>
+  .tab-content {
+    text-align: center;
+  }
+</style>
---
+++ pages/policy/email.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <h1>별별선생 이메일 집단 수집 거부</h1>
+    <h2>주식회사 티밸류와이즈 이메일 집단수집거부</h2>
+                
+    <p class="policy-content">우리 "별별선생"(주식회사 티밸류와이즈)의 웹사이트에 게시된 이메일 주소가 전자우편 수집 프로그램이나 그 밖의 기술적 장치를 이용하여 무단으로 수집되는 것을 거부하며, 이를 위반 시 정보통신망법에 의해 형사처벌 됨을 유념하시기 바랍니다.</p>
+                
+    <h3>정보통신망법 제 50조의 2 (전자우편주소의 무단 수집행위 등 금지)</h3>
+    <div class="index-line"></div>
+    <p class="policy-content">1. 누구든지 전자우편주소의 수집을 거부하는 의사가 명시된 인터넷 홈페이지에서 자동으로 전자우편주소를 수집하는 프로그램 그 밖의 기술적 장치를 이용하여 전자우편주소를 수집하여서는 아니 된다.</p>
+    <p class="policy-content">2. 누구든지 제1항의 규정을 위반하여 수집된 전자우편주소를 판매·유통하여서는 아니 된다.</p>
+    <p class="policy-content">3. 누구든지 제1항 및 제2항의 규정에 의하여 수집, 판매 및 유통이 금지된 전자우편 주소임을 알고 이를 정보전송에 이용하여서는 아니 된다.</p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 40px;
+}
+h2 {
+    font-size: 16px;
+    color: #666666;
+    text-align: center;
+    margin-bottom: 30px;
+}
+h3 {
+    font-size: 16px;
+    color: #212121;
+    text-align: left;
+}
+.policy-content {
+    font-size: 14px;
+    text-align: left;
+}
+.index-line {
+    border-bottom: 1px solid #dfdfdf;
+    margin-bottom: 12px;
+}
+.content-sub-title {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 0;
+    text-align: left;
+}
+</style>
