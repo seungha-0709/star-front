26b9cea920a5bedc39b94a11dba4d10fcf807d07
Seungha <sh.kim.x928@gmail.com>
2020-08-23 08:49:17 +0900

    Add: Q&A 질문 카테고리 아이콘 추가

--- components/qnaContents/qnaCategory.js
+++ components/qnaContents/qnaCategory.js
@@ -1,23 +1,30 @@
 export const qnaCategory = [
   {
-    name: "전체"
+    name: "전체",
+    icon: "search"
   },
   {
-    name: "주문/결제"
+    name: "주문/결제",
+    icon: "search"
   },
   {
-    name: "배송관련"
+    name: "배송관련",
+    icon: "search"
   },
   {
-    name: "환불/취소"
+    name: "환불/취소",
+    icon: "search"
   },
   {
-    name: "반품/교환"
+    name: "반품/교환",
+    icon: "search"
   },
   {
-    name: "영수증/증빙"
+    name: "영수증/증빙",
+    icon: ""
   },
   {
-    name: "서비스/기타"
+    name: "서비스/기타",
+    icon: ""
   }
 ]
--- pages/qna/index.vue
+++ pages/qna/index.vue
@@ -28,7 +28,9 @@
       <h2>자주 묻는 질문과 답변</h2>
       <ul class="qna-category">
         <li v-for="(item, index) in categoryData" :key="index">
-          <div class="icon"></div>
+          <div class="icon">
+            <v-icon>{{ item.icon }}</v-icon>
+          </div>
           {{ item.name }}
         </li>
       </ul>
@@ -47,78 +49,78 @@
 </template>
 
 <script>
-import { qnaCategory } from "../../components/qnaContents/qnaCategory.js"
-import { qnaList } from "../../components/qnaContents/qnaList.js"
+  import { qnaCategory } from "../../components/qnaContents/qnaCategory.js"
+  import { qnaList } from "../../components/qnaContents/qnaList.js"
 
-export default {
-  data() {
-    return {
-      categoryData: qnaCategory,
-      qnaList
+  export default {
+    data() {
+      return {
+        categoryData: qnaCategory,
+        qnaList
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.qna-body {
-  background-color: #f1f1f1;
-}
-.cs-center-wrap {
-  background-color: #fff;
-  width: 1200px;
-  height: 208px;
-  margin-left: auto;
-  margin-right: auto;
-  border: 1px solid #dfdfdf;
-  display: flex;
-  padding: 32px 40px 32px 40px;
-  box-sizing: border-box;
-}
-h2 {
-  font-size: 24px;
-  color: #212121;
-  margin-bottom: 24px;
-  font-weight: bold;
-}
-.cs-service {
-  width: 851px;
-}
-th {
-  text-align: left;
-  width: 100px;
-}
-.cs-online {
-  width: 348px;
-  border-left: 1px solid #dfdfdf;
-}
-.qna-wrap {
-  background-color: #fff;
-  width: 1200px;
-  margin-top: 8px;
-  margin-left: auto;
-  margin-right: auto;
-  border: 1px solid #dfdfdf;
-  padding: 32px 40px 32px 40px;
-}
-.qna-category {
-  list-style: none;
-  display: flex;
-  padding: 0;
-  width: 1180px;
-  margin-bottom: 40px;
-}
-.icon {
-  width: 56px;
-  height: 56px;
-  border-radius: 28px;
-  background-color: #dfdfdf;
-  margin: 24px 52px 8px 52px;
-}
-.qna-category > li {
-  border: 1px solid #dfdfdf;
-  width: 160px;
-  height: 136px;
-  text-align: center;
-}
+  .qna-body {
+    background-color: #f1f1f1;
+  }
+  .cs-center-wrap {
+    background-color: #fff;
+    width: 1200px;
+    height: 208px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dfdfdf;
+    display: flex;
+    padding: 32px 40px 32px 40px;
+    box-sizing: border-box;
+  }
+  h2 {
+    font-size: 24px;
+    color: #212121;
+    margin-bottom: 24px;
+    font-weight: bold;
+  }
+  .cs-service {
+    width: 851px;
+  }
+  th {
+    text-align: left;
+    width: 100px;
+  }
+  .cs-online {
+    width: 348px;
+    border-left: 1px solid #dfdfdf;
+  }
+  .qna-wrap {
+    background-color: #fff;
+    width: 1200px;
+    margin-top: 8px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dfdfdf;
+    padding: 32px 40px 32px 40px;
+  }
+  .qna-category {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    width: 1180px;
+    margin-bottom: 40px;
+  }
+  .icon {
+    width: 56px;
+    height: 56px;
+    border-radius: 28px;
+    background-color: #dfdfdf;
+    margin: 24px 52px 8px 52px;
+  }
+  .qna-category > li {
+    border: 1px solid #dfdfdf;
+    width: 160px;
+    height: 136px;
+    text-align: center;
+  }
 </style>
