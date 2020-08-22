7131946653615db9dd6f17101f36822121976a18
Seungha <sh.kim.x928@gmail.com>
2020-08-22 14:54:23 +0900

    Add: 자주묻는 질문과 답변 페이지

---
+++ pages/qna/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="qna-body">
+    <div class="cs-center-wrap">
+      <div class="cs-center">
+        <h2>고객센터</h2>
+        <table>
+          <tr>
+            <th>운영시간</th>
+            <td>평일 10:00~18:00 (점심시간 13:00~14:00 / 주말&공휴일 제외)</td>
+          </tr>
+          <tr>
+            <th>이메일</th>
+            <td>star_store@starteacher.co.kr</td>
+          </tr>
+          <tr>
+            <th>전화</th>
+            <td>070-1588-0000</td>
+          </tr>
+        </table>
+      </div>
+      <div class="cs-online">
+        <img src="" alt="" />
+        <v-btn outlined color="#1673e6">1:1 온라인 문의하기</v-btn>
+      </div>
+    </div>
+
+    <div class="qna-wrap">
+      <h2>자주 묻는 질문과 답변</h2>
+      <ul class="qna-category">
+        <li v-for="(data, index) in categoryData" :key="index">
+          <div class="icon"></div>
+          {{ data.name }}
+        </li>
+      </ul>
+
+      <v-expansion-panels>
+        <v-expansion-panel v-for="(item, i) in 5" :key="i">
+          <v-expansion-panel-header>Item</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        categoryData: [
+          {
+            name: "전체"
+          },
+          {
+            name: "주문/결제"
+          },
+          {
+            name: "배송관련"
+          },
+          {
+            name: "환불/취소"
+          },
+          {
+            name: "반품/교환"
+          },
+          {
+            name: "영수증/증빙"
+          },
+          {
+            name: "서비스/기타"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .qna-body {
+    background-color: #f1f1f1;
+  }
+  .cs-center-wrap {
+    background-color: #fff;
+    width: 1200px;
+    height: 208px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dfdfdf;
+    display: flex;
+    padding: 32px 40px 32px 40px;
+    box-sizing: border-box;
+  }
+  .cs-center {
+    width: 851px;
+  }
+  .cs-online {
+    width: 348px;
+    border-left: 1px solid #dfdfdf;
+  }
+  .qna-wrap {
+    background-color: #fff;
+    width: 1200px;
+    margin-top: 8px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dfdfdf;
+    padding: 32px 40px 32px 40px;
+  }
+  .qna-category {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    width: 1180px;
+  }
+  .icon {
+    width: 56px;
+    height: 56px;
+    border-radius: 28px;
+    background-color: #dfdfdf;
+    margin: 24px 52px 8px 52px;
+  }
+  .qna-category > li {
+    border: 1px solid #dfdfdf;
+    width: 160px;
+    height: 136px;
+    text-align: center;
+  }
+</style>
