fe9e95543ed965b86d591330b664cf2aec27e18e
dongmin <ehdals0227@gmail.com>
2020-08-25 20:22:00 +0900

    Fix: 재가공된 data를 다른 컴포넌트에 props로 받게함

--- components/store/cartBreakdown.vue
+++ components/store/cartBreakdown.vue
@@ -1,47 +1,56 @@
 <template>
   <div class="content">
     <div>
-<table class="breakdown-table">
-  <tbody>
-    <tr>
-      <td>수량</td>
-      <td>상품금액</td>
-      <td>할인금액</td>
-      <td>배송비</td>
-    </tr>
-    <tr>
-      <td>전체 주문금액</td>
-    </tr>
-  </tbody>
-</table>
-<button class="order-btn">주문하기</button>
+      <table class="breakdown-table">
+        <tbody>
+          <tr>
+            <td>수량</td>
+            <td>상품금액</td>
+            <td>할인금액</td>
+            <td>배송비</td>
+          </tr>
+          <tr>
+            <td>전체 주문금액</td>
+          </tr>
+        </tbody>
+      </table>
+      <button class="order-btn">주문하기</button>
     </div>
-
   </div>
 </template>
 <script>
-export default {
-  
-}
+  export default {
+    props: ["result-data"],
+    data() {
+      return {
+        paymentData: []
+      }
+    },
+    watch: {
+      resultData() {
+        this.paymentData = this.resultData
+      }
+    }
+  }
 </script>
 <style scoped>
-.breakdown-table {
-  width: 1120px;
-  height: 250px;
-  margin-top: 48px;
-}
-.order-btn {
-  width: 240px;
-  height: 52px;
-  border-radius: 26px;
-  background-color: #1673e6;
-  font-size: 16px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: center;
-  color: #ffffff;
-}
+  .breakdown-table {
+    width: 1120px;
+    height: 250px;
+    margin-top: 48px;
+  }
+  .order-btn {
+    width: 240px;
+    height: 52px;
+    border-radius: 26px;
+    background-color: #1673e6;
+    font-size: 16px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: #ffffff;
+  }
 </style>
