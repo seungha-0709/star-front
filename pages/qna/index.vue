6a6fffd23de06823fb6d0a6e82b4c1e7e81795c3
Seungha <sh.kim.x928@gmail.com>
2020-08-26 18:48:22 +0900

    Fix: 변경 사항 반영

--- pages/qna/index.vue
+++ pages/qna/index.vue
@@ -7,7 +7,9 @@
           <tbody>
             <tr>
               <th>운영시간</th>
-              <td>평일 10:00~18:00 (점심시간 13:00~14:00 / 주말&공휴일 제외)</td>
+              <td>
+                평일 10:00~18:00 (점심시간 13:00~14:00 / 주말&공휴일 제외)
+              </td>
             </tr>
             <tr>
               <th>이메일</th>
@@ -21,7 +23,7 @@
         </table>
       </div>
       <div class="cs-online">
-        <v-icon size="80" color="#ececec">headset</v-icon>
+        <headphones-icon class="headphone-icon" size="78" color="#ececec" />
         <div class="btn">1:1 온라인 문의하기</div>
       </div>
     </div>
@@ -36,9 +38,38 @@
           @click="onTypeChange(item.type)"
         >
           <div class="icon-bg">
-            <div class="icon">
-              <v-icon color="#8f8f8f">{{ item.icon }}</v-icon>
-            </div>
+            <span v-if="item.type === 'all'" class="icon-all">
+              ALL
+            </span>
+            <span v-if="item.type === 'buy'" class="icon-buy">
+              <span>W</span>
+            </span>
+            <map-pin-icon
+              v-if="item.type === 'logistics'"
+              size="24"
+              class="icon-style"
+            />
+            <package-icon
+              v-if="item.type === 'cancel'"
+              size="24"
+              class="icon-style"
+            />
+            <repeat-icon
+              v-if="item.type === 'exchange'"
+              size="24"
+              class="icon-style"
+            />
+            <file-text-icon
+              v-if="item.type === 'receipt'"
+              size="24"
+              class="icon-style"
+            />
+            <heart-icon
+              v-if="item.type === 'etc'"
+              size="24"
+              class="icon-style"
+            />
+            <!-- <v-icon color="#8f8f8f">{{ item.icon }}</v-icon> -->
           </div>
           {{ item.name }}
         </li>
@@ -49,128 +80,183 @@
 </template>
 
 <script>
-import { qnaCategory } from "../../components/qnaContents/qnaCategory.js"
-import qnaTab from "../../components/qnaContents/qnaTap.vue"
+  import { qnaCategory } from "../../components/qnaContents/qnaCategory.js"
+  import qnaTab from "../../components/qnaContents/qnaTap.vue"
+  import {
+    HeadphonesIcon,
+    MapPinIcon,
+    PackageIcon,
+    RepeatIcon,
+    FileTextIcon,
+    HeartIcon
+  } from "vue-feather-icons"
 
-export default {
-  data() {
-    return {
-      categoryData: qnaCategory,
-      activeType: "all"
-    }
-  },
-  components: {
-    qnaTab
-  },
-  methods: {
-    onTypeChange(type) {
-      this.activeType = type
+  export default {
+    data() {
+      return {
+        categoryData: qnaCategory,
+        activeType: "all"
+      }
+    },
+    components: {
+      qnaTab,
+      HeadphonesIcon,
+      MapPinIcon,
+      PackageIcon,
+      RepeatIcon,
+      FileTextIcon,
+      HeartIcon
+    },
+    methods: {
+      onTypeChange(type) {
+        this.activeType = type
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.qna-body {
-  background-color: #f1f1f1;
-  overflow: hidden;
-}
-.cs-center-wrap {
-  background-color: #fff;
-  width: 1200px;
-  height: 208px;
-  margin-top: 40px;
-  margin-left: auto;
-  margin-right: auto;
-  border: 1px solid #dfdfdf;
-  display: flex;
-  padding: 32px 40px 32px 40px;
-  box-sizing: border-box;
-}
-h2 {
-  font-size: 24px;
-  color: #212121;
-  margin-bottom: 24px;
-  font-weight: bold;
-}
-/* 고객센터 */
-.cs-service {
-  width: 851px;
-}
-tbody {
-  font-size: 14px;
-}
-th {
-  text-align: left;
-  width: 100px;
-  font-weight: bold;
-  color: #212121;
-}
-.cs-online {
-  width: 348px;
-  border-left: 1px solid #dfdfdf;
-  text-align: center;
-}
-.btn {
-  width: 200px;
-  height: 42px;
-  border-radius: 4px;
-  border: solid 1px #1673e6;
-  background-color: #ffffff;
-  margin: 10px auto 41px auto;
-  font-size: 16px;
-  font-weight: bold;
-  line-height: 2.6;
-  color: #1673e6;
-}
-/* 자주 묻는 질문과 답변 */
-.qna-wrap {
-  background-color: #fff;
-  color: #212121;
-  width: 1200px;
-  margin: 8px auto 0px auto;
-  border: 1px solid #dfdfdf;
-  padding: 32px 40px 32px 40px;
-}
-.qna-category {
-  list-style: none;
-  display: flex;
-  padding: 0;
-  width: 1180px;
-  margin-bottom: 40px;
-  color: #212121;
-}
-.icon-bg {
-  width: 56px;
-  height: 56px;
-  border-radius: 28px;
-  background-color: rgba(223, 223, 223, 0.2);
-  margin: 24px 52px 8px 52px;
-  padding: 16px;
-}
+  .qna-body {
+    background-color: #f1f1f1;
+    overflow: hidden;
+  }
+  .cs-center-wrap {
+    background-color: #fff;
+    width: 1200px;
+    height: 208px;
+    margin-top: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #dfdfdf;
+    display: flex;
+    padding: 32px 40px 32px 40px;
+    box-sizing: border-box;
+  }
+  h2 {
+    font-size: 24px;
+    color: #212121;
+    margin-bottom: 24px;
+    font-weight: bold;
+  }
+  /* 고객센터 */
+  .cs-service {
+    width: 851px;
+  }
+  tbody {
+    font-size: 14px;
+  }
+  th {
+    text-align: left;
+    width: 100px;
+    font-weight: bold;
+    color: #212121;
+  }
+  .cs-online {
+    width: 348px;
+    border-left: 1px solid #dfdfdf;
+    text-align: center;
+  }
+  .cs-online .headphone-icon {
+    margin-top: 4px;
+    vertical-align: top;
+  }
+  .btn {
+    width: 200px;
+    height: 42px;
+    border-radius: 4px;
+    border: solid 1px #1673e6;
+    background-color: #ffffff;
+    margin: 10px auto 41px auto;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 2.6;
+    color: #1673e6;
+  }
+  /* 자주 묻는 질문과 답변 */
+  .qna-wrap {
+    background-color: #fff;
+    color: #212121;
+    width: 1200px;
+    margin: 8px auto 0px auto;
+    border: 1px solid #dfdfdf;
+    padding: 32px 40px 32px 40px;
+  }
+  .qna-category {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    width: 1180px;
+    margin-bottom: 40px;
+    color: #212121;
+  }
+  .icon-bg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 56px;
+    height: 56px;
+    border-radius: 28px;
+    background-color: rgba(223, 223, 223, 0.2);
+    margin: 24px 52px 8px 52px;
+    padding: 16px;
+  }
 
-.qna-category > li {
-  border-top: 1px solid #dfdfdf;
-  border-bottom: 1px solid #dfdfdf;
-  border-left: 1px solid #dfdfdf;
-  width: 160px;
-  height: 136px;
-  text-align: center;
-  cursor: pointer;
-  box-sizing: border-box;
-  color: #212121;
-  font-size: 16px;
-}
-.qna-category > li:last-child {
-  border-right: 1px solid #dfdfdf;
-}
-.qna-category > li.active {
-  border: 1px solid #dfdfdf;
-  width: 160px;
-  height: 136px;
-  text-align: center;
-  background-color: #1673e6;
-  box-sizing: border-box;
-  color: white;
-}
+  .icon-bg > .icon-all {
+    font-size: 16px;
+    color: #8f8f8f;
+    font-weight: 700;
+  }
+  .icon-bg > .icon-buy {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 22px;
+    height: 22px;
+    border: 2px solid #8f8f8f;
+    border-radius: 50%;
+  }
+  .icon-bg > .icon-buy > span {
+    width: 10px;
+    font-size: 13px;
+    font-weight: 700;
+    color: #8f8f8f;
+  }
+  .icon-bg > .icon-style {
+    color: #8f8f8f;
+  }
+
+  .qna-category > li {
+    border-top: 1px solid #dfdfdf;
+    border-bottom: 1px solid #dfdfdf;
+    border-left: 1px solid #dfdfdf;
+    width: 160px;
+    height: 136px;
+    text-align: center;
+    cursor: pointer;
+    box-sizing: border-box;
+    color: #212121;
+    font-size: 16px;
+  }
+  .qna-category > li:last-child {
+    border-right: 1px solid #dfdfdf;
+  }
+  .qna-category > li.active {
+    border: 1px solid #dfdfdf;
+    width: 160px;
+    height: 136px;
+    text-align: center;
+    background-color: #1673e6;
+    box-sizing: border-box;
+    color: white;
+  }
+
+  .qna-category > li.active .icon-bg > .icon-all,
+  .qna-category > li.active .icon-bg > .icon-style {
+    color: white;
+  }
+  .qna-category > li.active .icon-bg > .icon-buy,
+  .qna-category > li.active .icon-bg > .icon-buy > span {
+    color: white;
+    border-color: white;
+  }
 </style>
