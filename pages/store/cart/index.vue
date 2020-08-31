99c7ff70d63e42338b565dd1f0ca475f51fd42fc
dongmin <ehdals0227@gmail.com>
2020-08-31 15:53:52 +0900

    Fix: 해당 컴포넌트에서 data부르던것 index에서 받아 props로 제어 / 이벤트 분리

--- pages/store/cart/index.vue
+++ pages/store/cart/index.vue
@@ -4,47 +4,50 @@
       <!-- <cart-item v-on:sendResultData="getResultData" /> -->
       <cart-breakdown v-bind:result-data="resultData" />
       <order-button />
-      <cart-cast />
+      <cart-cast v-on:sendResultData="getResultData" :cart-lists="cartLists" />
       <!-- <order-sheet /> -->
     </div>
   </div>
 </template>
 <script>
-import cartEmpty from "../../../components/store/cartEmpty.vue"
-import cartItem from "../../../components/store/cartItem.vue"
-import cartBreakdown from "../../../components/store/cartBreakdown.vue"
-import orderButton from "../../../components/store/orderButton.vue"
-import cartCast from "../../../components/store/cartCast.vue"
-import orderSheet from "../../../components/store/orderSheet.vue"
+  import cartEmpty from "../../../components/store/cartEmpty.vue"
+  import cartItem from "../../../components/store/cartItem.vue"
+  import cartBreakdown from "../../../components/store/cartBreakdown.vue"
+  import orderButton from "../../../components/store/orderButton.vue"
+  import cartCast from "../../../components/store/cartCast.vue"
+  import orderSheet from "../../../components/store/orderSheet.vue"
 
-export default {
-  data() {
-    return {
-      resultData: []
-    }
-  },
-  components: {
-    "cart-item": cartItem,
-    "cart-breakdown": cartBreakdown,
-    "cart-empty": cartEmpty,
-    "order-button": orderButton,
-    "cart-cast": cartCast,
-    "order-sheet": orderSheet
-  },
-  methods: {
-    getResultData(data) {
-      this.resultData = data
+  import { cartLists } from "../../../components/store/cartLists.js"
+
+  export default {
+    data() {
+      return {
+        cartLists,
+        resultData: []
+      }
+    },
+    components: {
+      "cart-item": cartItem,
+      "cart-breakdown": cartBreakdown,
+      "cart-empty": cartEmpty,
+      "order-button": orderButton,
+      "cart-cast": cartCast,
+      "order-sheet": orderSheet
+    },
+    methods: {
+      getResultData(data) {
+        this.resultData = data
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.cart-page {
-  width: 1200px;
-  margin: 32px auto 0;
-  border: 1px solid #dfdfdf;
-  background-color: #ffffff;
-  text-align: center;
-}
+  .cart-page {
+    width: 1200px;
+    margin: 32px auto 0;
+    border: 1px solid #dfdfdf;
+    background-color: #ffffff;
+    text-align: center;
+  }
 </style>
