e753a36d9a7ff7af504f1ec102fd4ee878183c23
dongmin <ehdals0227@gmail.com>
2020-09-03 18:46:41 +0900

    Add: basicButton 컴포넌트 추가

---
+++ components/common/basicButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    type="button"
+    :style="{
+      color,
+      backgroundColor,
+      fontSize: fontSize + 'px',
+      fontWeight,
+      border,
+      borderRadius: borderRadius + 'px',
+      width: width + 'px',
+      height: height + 'px'
+    }"
+    @click="$emit('event')"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      text: {
+        default: ""
+      },
+      color: {
+        default: "#fff"
+      },
+      fontSize: {
+        default: 16
+      },
+      fontWeight: {
+        default: 700
+      },
+      backgroundColor: {
+        default: "#1673e6"
+      },
+      borderColor: {
+        default: ""
+      },
+      borderRadius: {
+        default: 26
+      },
+      width: {
+        default: 240
+      },
+      height: {
+        default: 52
+      }
+    },
+    computed: {
+      border() {
+        return this.borderColor ? `1px solid ${this.borderColor}` : 0
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
