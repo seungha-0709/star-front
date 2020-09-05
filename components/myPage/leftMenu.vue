f454480e586da3dd7942bcf9a558761d8213f057
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-05 18:08:04 +0900

    Add: myPage components, pages

--- components/myPage/leftMenu.vue
+++ components/myPage/leftMenu.vue
@@ -2,8 +2,8 @@
   <div class="content-menu">
     <div class="rectangle">
       <div class="title">
-        <span class="menu-icon">img</span>
-        <h4 class="my-page">마이페이지</h4>
+        <span class="menu-icon"><user-icon class="user-icon" /></span>
+        <h4 class="my-page-title">마이페이지</h4>
       </div>
       <div>
         <h5 class="sub-title sub-menu">프로필</h5>
@@ -13,49 +13,61 @@
     </div>
   </div>
 </template>
-
 <script>
-export default {
-  data() {
-    return {}
+  import { UserIcon } from "vue-feather-icons"
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      UserIcon
+    }
   }
-}
 </script>
 
 <style scoped>
-.content-menu {
-  width: 260px;
-  height: 240px;
-  border: solid 1px #dfdfdf;
-  background-color: #ffffff;
-  font-family: SpoqaHanSans;
-  font-size: 16px;
-  font-weight: bold;
-  text-align: left;
-  color: #212121;
-}
-.content-menu .rectangle {
-  width: 100%;
-  height: 100%;
-  padding: 32px 24px 8px;
-}
-.content-menu .title {
-  margin-bottom: 8px;
-}
-.my-page {
-  font-size: 24px;
-  font-weight: normal;
-  color: #666666;
-  margin-left: 8px;
-}
-.content-menu .sub-title {
-  width: 212px;
-  border-bottom: solid 1px #ececec;
-}
-/* .sub-menu {
-} */
-.content-menu .menu-icon {
-  width: 24px;
-  height: 27px;
-}
+  .content-menu {
+    width: 260px;
+    height: 240px;
+    border: solid 1px #dfdfdf;
+    background-color: #ffffff;
+    margin: 0 16px 8px 0;
+    font-family: SpoqaHanSans;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: left;
+    color: #212121;
+  }
+  .content-menu .rectangle {
+    width: 100%;
+    height: 100%;
+    padding: 32px 24px 8px;
+  }
+  .content-menu .title {
+    margin-bottom: 8px;
+  }
+  .my-page-title {
+    display: inline-block;
+    font-size: 24px;
+    font-weight: normal;
+    color: #666666;
+    margin-left: 8px;
+  }
+  .content-menu .sub-title {
+    width: 212px;
+    border-bottom: solid 1px #ececec;
+  }
+  .sub-menu {
+    height: 52px;
+    padding: 14px 0 14px;
+  }
+  .content-menu .menu-icon {
+    width: 24px;
+    height: 36px;
+    padding: 4px 0 5px;
+  }
+  .content-menu .user-icon {
+    color: #dfdfdf;
+  }
 </style>
---
+++ pages/mypage/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="my-page">
+      <left-menu />
+      <notification-content />
+    </div>
+  </div>
+</template>
+<script>
+  import leftMenu from "../../components/myPage/leftMenu.vue"
+  import notificationCast from "../../components/myPage/notificationCast"
+  import notificationContents from "../../components/myPage/notificationContents.vue"
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      "left-menu": leftMenu,
+      "notification-cast": notificationCast,
+      "notification-content": notificationContents
+    }
+  }
+</script>
+<style scoped>
+  .my-page {
+    width: 1200px;
+    margin: 32px auto 0;
+    text-align: center;
+  }
+</style>
