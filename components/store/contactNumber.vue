00627f80e1f583c2cd75b2d841e6c83c7eb782ed
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-06 16:43:36 +0900

    Edit: consol.log 등의 주석 삭제

--- components/store/contactNumber.vue
+++ components/store/contactNumber.vue
@@ -7,7 +7,6 @@
           <input class="contact-input" type="number" placeholder="010" />
           <input class="contact-input" type="number" placeholder="1234" />
           <input class="contact-input" type="number" placeholder="1234" />
-          <!-- placeholder 말고 text 로 해야할듯..? 우선 placeholder custom을 더 찾아보자능..  -->
           <button class="verify-num">인증번호 받기</button>
         </div>
       </div>
@@ -20,81 +19,86 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {}
+  export default {
+    data() {
+      return {}
+    }
   }
-}
 </script>
 
 <style scoped>
-.contact-content {
-  width: 100%;
-  height: 156px;
-  margin: 0 auto;
-  font-family: SpoqaHanSans;
-  font-weight: normal;
-  color: #212121;
-  vertical-align: middle;
-  text-align: left;
-  font-size: 16px;
-  background-color: #ffffff;
-}
-.contact {
-  font-weight: bold;
-  padding: 24px 0;
-  border-bottom: solid 1px #dfdfdf;
-}
-.contact input {
-  height: 46px;
-  border-radius: 4px;
-  border: solid 1px #dfdfdf;
-  margin-right: 8px;
-  padding-left: 16px;
-}
-.contact input::placeholder {
-  color: #c1c1c1;
-  text-align: left;
-}
-.contact-number {
-  display: flex;
-  text-align: center;
-  height: 46px;
-  margin-bottom: 16px;
-}
-.phone {
-  height: 46px;
-  margin: 11px 4px 0 0;
-}
-.input-box {
-  margin-left: 44px;
-}
-.contact-input {
-  width: 80px;
-  text-align: center;
-}
-.verify-num {
-  margin-left: 8px;
-  width: 124px;
-  height: 46px;
-  border-radius: 4px;
-  border: solid 1px #1673e6;
-  color: #1673e6;
-}
+  .contact-content {
+    width: 100%;
+    height: 156px;
+    margin: 0 auto;
+    font-family: SpoqaHanSans;
+    font-weight: normal;
+    color: #212121;
+    vertical-align: middle;
+    text-align: left;
+    font-size: 16px;
+    background-color: #ffffff;
+  }
+  .contact {
+    font-weight: bold;
+    padding: 24px 0;
+    border-bottom: solid 1px #dfdfdf;
+  }
+  .contact input {
+    height: 46px;
+    border-radius: 4px;
+    border: solid 1px #dfdfdf;
+    margin-right: 8px;
+    padding-left: 16px;
+  }
+  .input-box input::placeholder {
+    color: #c1c1c1;
+    text-align: center;
+  }
+  .contact-number {
+    display: flex;
+    text-align: center;
+    height: 46px;
+    margin-bottom: 16px;
+  }
+  .phone {
+    height: 46px;
+    margin: 11px 4px 0 0;
+  }
+  .input-box {
+    margin-left: 44px;
+  }
+  .contact-input {
+    width: 80px;
+    text-align: center;
+  }
+  .verify-num {
+    margin-left: 8px;
+    width: 124px;
+    height: 46px;
+    border-radius: 4px;
+    border: solid 1px #1673e6;
+    color: #1673e6;
+  }
 
-.verify {
-  display: flex;
-  margin-left: 118px;
-}
-.verify-input {
-  width: 200px;
-}
-.verifying {
-  width: 91px;
-  height: 46px;
-  border-radius: 4px;
-  color: #8f8f8f;
-  background-color: #ececec;
-  margin-left: 8px;
-}
+  .verify {
+    display: flex;
+    margin-left: 118px;
+  }
+  .verify-input {
+    width: 200px;
+    text-align: left;
+  }
+  .verify input::placeholder {
+    text-align: left;
+    color: #c1c1c1;
+  }
+  .verifying {
+    width: 91px;
+    height: 46px;
+    border-radius: 4px;
+    color: #8f8f8f;
+    background-color: #ececec;
+    margin-left: 8px;
+  }
 </style>
