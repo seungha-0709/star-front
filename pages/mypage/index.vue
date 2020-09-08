d2197f1b9327af0aad11cbc401e75fb19c61d8c5
dongmin <ehdals0227@gmail.com>
2020-09-08 16:02:59 +0900

    Fix: leftmenu 컴포넌트 적용 / 아이콘은 별개로 넘겨줘야 하는 이슈가 있음.

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <div class="my-page">
-      <left-menu />
-      <left-menu />
+      <left-menu title="마이페이지" :menu="myPageMenu">
+        <user-icon size="24" color="#dfdfdf" />
+      </left-menu>
+      <left-menu title="나의 쇼핑" :menu="myShoppingMenu">
+        <shopping-bag-icon size="24" color="#dfdfdf" />
+      </left-menu>
       <!-- <notification-content /> -->
     </div>
   </div>
@@ -11,14 +15,23 @@
   import leftMenu from "../../components/myPage/leftMenu.vue"
   import notificationCast from "../../components/myPage/notificationCast"
   import notificationContents from "../../components/myPage/notificationContents.vue"
+
+  import { myPageMenu, myShoppingMenu } from "../../assets/data/sidebar"
+  import { UserIcon, ShoppingBagIcon } from "vue-feather-icons"
+
   export default {
     data() {
-      return {}
+      return {
+        myPageMenu,
+        myShoppingMenu
+      }
     },
     components: {
       "left-menu": leftMenu,
       "notification-cast": notificationCast,
-      "notification-content": notificationContents
+      "notification-content": notificationContents,
+      UserIcon,
+      ShoppingBagIcon
     }
   }
 </script>
