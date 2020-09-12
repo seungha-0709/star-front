c9f0a2c86719497b90625b786cab8f5e939f3557
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-12 23:08:37 +0900

    Add: status.vue > 로그아웃 버튼

---
+++ components/common/status.vue
@@ -0,0 +1,14 @@
+<template>
+  <power-icon size="8" class=""></power-icon>
+</template>
+
+<script>
+  import { PowerIcon } from "vue-feather-icons"
+
+  export default {
+    components: {
+      PowerIcon
+    }
+  }
+</script>
+<style scoped></style>
--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -8,6 +8,7 @@
         <sidebar-menu title="나의 쇼핑" :menu="myShoppingMenu">
           <shopping-bag-icon size="24" color="#dfdfdf" />
         </sidebar-menu>
+        <status />
       </div>
       <div class="notification">
         <notification-content />
@@ -19,6 +20,7 @@
   import sidebarMenu from "../../components/common/sidebarMenu.vue"
   import notificationCast from "../../components/myPage/notificationCast"
   import notificationContents from "../../components/myPage/notificationContents.vue"
+  import status from "../../components/common/status.vue"
 
   import { myPageMenu, myShoppingMenu } from "../../assets/data/sidebar"
   import { UserIcon, ShoppingBagIcon } from "vue-feather-icons"
@@ -34,6 +36,7 @@
       "sidebar-menu": sidebarMenu,
       "notification-cast": notificationCast,
       "notification-content": notificationContents,
+      status: status,
       UserIcon,
       ShoppingBagIcon
     }
