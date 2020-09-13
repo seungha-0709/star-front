decd91bbdb2ecaefd5ac619b63befe50656fd106
Seungha <sh.kim.x928@gmail.com>
2020-09-13 18:25:45 +0900

    Fix: 페이지 이전/다음 아이콘 cursor: pointer; 추가

--- components/orderList/pagination.vue
+++ components/orderList/pagination.vue
@@ -2,6 +2,7 @@
   <div>
     <div v-if="pageSetting.list.length" class="page-list">
       <chevrons-left-icon
+        class="left-icon"
         v-if="pageSetting.first !== null"
         color="#dfdfdf"
         @click="pageSetting.first !== null ? sendPage(pageSetting.first) : ''"
@@ -12,9 +13,12 @@
           v-for="page in pageSetting.list"
           :key="page"
           @click="sendPage(page)"
-        >{{ page }}</li>
+        >
+          {{ page }}
+        </li>
       </ul>
       <chevrons-right-icon
+        class="right-icon"
         v-if="pageSetting.end !== null"
         color="#dfdfdf"
         @click="pageSetting.end !== null ? sendPage(pageSetting.end) : ''"
@@ -24,42 +28,48 @@
 </template>
 
 <script>
-import { ChevronsLeftIcon, ChevronsRightIcon } from "vue-feather-icons"
-export default {
-  props: ["pageSetting", "pageBlock"],
+  import { ChevronsLeftIcon, ChevronsRightIcon } from "vue-feather-icons"
+  export default {
+    props: ["pageSetting", "pageBlock"],
 
-  components: {
-    ChevronsLeftIcon,
-    ChevronsRightIcon
-  },
-  methods: {
-    sendPage(page) {
-      this.$emit("paging", page)
+    components: {
+      ChevronsLeftIcon,
+      ChevronsRightIcon
+    },
+    methods: {
+      sendPage(page) {
+        this.$emit("paging", page)
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.page-list {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.page-list ul {
-  list-style: none;
-  display: flex;
-}
-.page-list li {
-  font-size: 14px;
-  color: #666;
-  padding: 12px;
-  cursor: pointer;
-}
-.page-list li.active {
-  font-size: 14px;
-  color: #2c428d;
-  font-weight: bold;
-  cursor: pointer;
-}
+  .page-list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .page-list ul {
+    list-style: none;
+    display: flex;
+  }
+  .page-list li {
+    font-size: 14px;
+    color: #666;
+    padding: 12px;
+    cursor: pointer;
+  }
+  .page-list li.active {
+    font-size: 14px;
+    color: #2c428d;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .left-icon {
+    cursor: pointer;
+  }
+  .right-icon {
+    cursor: pointer;
+  }
 </style>
