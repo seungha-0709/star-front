ccae7485e925a4899f1d48f012961986996e7a55
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-14 23:00:45 +0900

    Edit:status.vue style

--- components/common/status.vue
+++ components/common/status.vue
@@ -1,5 +1,10 @@
 <template>
-  <power-icon size="8" class=""></power-icon>
+  <div class="status-show">
+    <div class="off-icon">
+      <power-icon size="8" class="power-icon"></power-icon>
+    </div>
+    <a class="status-text" href="#">로그아웃</a>
+  </div>
 </template>
 
 <script>
@@ -11,4 +16,31 @@
     }
   }
 </script>
-<style scoped></style>
+<style scoped>
+  .status-show {
+    display: flex;
+    height: 20px;
+    margin-top: 10px;
+    font-size: 14px;
+    text-align: center;
+    vertical-align: middle;
+  }
+  .off-icon {
+    display: block;
+    width: 16px;
+    height: 16px;
+    border-radius: 8px;
+    background-color: #dfdfdf;
+    margin-right: 4px;
+  }
+  .power-icon {
+    position: relative;
+    top: -3px;
+    color: #666666;
+  }
+  .status-text {
+    position: relative;
+    color: #666666;
+    top: -3px;
+  }
+</style>
