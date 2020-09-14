3b5c844ec0eafa965c65d34a013e4e080d85824d
dongmin <ehdals0227@gmail.com>
2020-09-14 17:40:15 +0900

    Add: vuex store에 장바구니 관련 액션 설정

--- store/index.js
+++ store/index.js
@@ -1,9 +1,35 @@
+// 값을 저장하는 객체
 export const state = () => ({
-  sidebar: false
+  cartLists: []
 })
 
+// state의 값을 반환하는 함수, computed 속성
+export const getters = {
+  GE_CART_LISTS: (state) => state.cartLists
+}
+
+// state의 값 변경하는 함수, setter
 export const mutations = {
-  toggleSidebar(state) {
-    state.sidebar = !state.sidebar
+  MU_CART_LISTS(state, payload) {
+    state.cartLists = payload
+  },
+  MU_CART_PRODUCT_AMOUNT_UP(state, payload) {
+    state.cartLists[payload].amount += 1
+  },
+  MU_CART_PRODUCT_AMOUNT_DOWN(state, payload) {
+    state.cartLists[payload].amount -= 1
+  }
+}
+
+// mutations에 값 보내는 함수
+export const actions = {
+  async AC_CART_LISTS({ commit }, payload) {
+    await commit("MU_CART_LISTS", payload)
+  },
+  async AC_CART_PRODUCT_AMOUNT_UP({ commit }, payload) {
+    await commit("MU_CART_PRODUCT_AMOUNT_UP", payload)
+  },
+  async AC_CART_PRODUCT_AMOUNT_DOWN({ commit }, payload) {
+    await commit("MU_CART_PRODUCT_AMOUNT_DOWN", payload)
   }
 }
