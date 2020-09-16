ff4568f3e180405a22b3013e237f306429118a43
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-16 17:56:42 +0900

    Add: myShoppingCast.vue

---
+++ components/common/myShoppingCast.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="my-shopping">
+    <div>
+      <h3 class="shopping-menu">{{ title }}</h3>
+    </div>
+    <div class="content-status">
+      <p class="content-count">{{ `총 ${(123).toLocaleString()}개` }}</p>
+      <basic-button
+        text="모두삭제"
+        width="84"
+        height="42"
+        color="#212121"
+        fontSize="14"
+        borderRadius="4"
+        backgroundColor="#ffffff"
+        borderColor="#dfdfdf"
+        fontWeight="400"
+      />
+    </div>
+    <div>
+      <table class="my-shopping-table">
+        <thead>
+          <tr class="table-header">
+            <th class="th-num">{{ thFirst }}</th>
+            <th class="th-date">{{ thSecond }}</th>
+            <th class="th-title">{{ thThird }}</th>
+            <th class="th-date">{{ thFourth }}</th>
+            <th class="th-date">{{ thFifth }}</th>
+          </tr>
+          <!-- vertical-align middle 했는데 왜 세로중앙정렬이 안되죠? -->
+        </thead>
+      </table>
+    </div>
+    <div>
+      <!-- <page-nation /> -->
+      <!-- list error 발생! -->
+    </div>
+  </div>
+</template>
+<script>
+  import basicButton from "../common/basicButton.vue"
+  import pagination from "../orderList/pagination.vue"
+  export default {
+    props: {
+      title: {
+        default: ""
+      },
+      thFirst: {
+        default: ""
+      },
+      thSecond: {
+        default: ""
+      },
+      thThird: {
+        default: ""
+      },
+      thFourth: {
+        default: ""
+      },
+      thFifth: {
+        default: ""
+      }
+    },
+
+    components: {
+      "basic-button": basicButton,
+      "page-nation": pagination
+    }
+  }
+</script>
+
+<style scoped>
+  .my-shopping {
+    width: 924px;
+    height: 696px;
+    border: solid 1px #dfdfdf;
+    background-color: #ffffff;
+    padding: 32px 40px 40px;
+    font-size: 14px;
+    font-weight: normal;
+    color: #212121;
+    vertical-align: middle;
+  }
+  .shopping-menu {
+    text-align: left;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 2px;
+  }
+  .content-status {
+    display: flex;
+    margin-bottom: 14px;
+  }
+  .content-count {
+    margin-top: 22px;
+    margin-right: 706px;
+  }
+  .my-shopping-table {
+    width: 844px;
+    text-align: center;
+    margin-bottom: 32px;
+    font-size: 15px;
+  }
+  .table-header {
+    width: 100%;
+    height: 40px;
+    border-top: solid 1px #666666;
+    vertical-align: middle;
+  }
+  .my-shopping tr {
+    width: 100%;
+    border-bottom: solid 1px #dfdfdf;
+    vertical-align: middle;
+  }
+  .table-header .th-num {
+    width: 60px;
+  }
+  .table-header .th-date {
+    width: 140px;
+  }
+  .table-header .th-title {
+    width: 364px;
+    text-align: left;
+  }
+</style>
--- pages/mypage/notification/index.vue
+++ pages/mypage/notification/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="notification-page">
-    <div>
-      <div class="notification">
-        <notification-content />
-      </div>
+    <div class="my-shopping-cast">
+      <my-shopping-cast />
     </div>
+    <!-- <div class="notification">
+      <notification-content />
+    </div> -->
   </div>
 </template>
 <script>
+  import myShoppingCast from "../../../components/common/myShoppingCast.vue"
+  // 나의 쇼핑 메뉴 테이블 공통 캄포넌트
   import notificationContents from "../../../components/myPage/notificationContents"
   // 나의 쇼핑 메뉴 중 알림 테이블 콘텐츠 컴포넌트
   export default {
@@ -15,12 +18,16 @@
       return {}
     },
     components: {
-      "notification-content": notificationContents
+      "notification-content": notificationContents,
+      "my-shopping-cast": myShoppingCast
     }
   }
 </script>
 <style scoped>
   .notification-page .notification {
     display: block;
+  }
+  .notification-page .my-shopping-cast {
+    margin: 32px 0 0 16px;
   }
 </style>
