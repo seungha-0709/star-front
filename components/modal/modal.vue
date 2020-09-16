18ad8215ea45148a8a1e018bdae0f9f16bd83453
dongmin <ehdals0227@gmail.com>
2020-09-16 19:58:11 +0900

    Fix: 모달 on/off시 다시 안열리는 이슈 수정

--- components/modal/modal.vue
+++ components/modal/modal.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="bg" v-if="modalDisplay === true">
+  <div class="bg">
     <div class="modal-bg" v-if="modalProps.isDimmed === true">
       <div class="modal-container">
-        <div class="close-icon-box" v-if="modalProps.isClose === true" @click="onClose">
+        <div
+          class="close-icon-box"
+          v-if="modalProps.isClose === true"
+          @click="$emit('close')"
+        >
           <x-icon class="close-icon" />
         </div>
         <div class="modal-wrap">
           <div class="modal-header">
             <h1>{{ modalProps.title }}</h1>
             <h3>{{ modalProps.subTitle }}</h3>
-            <div class="modal-divider" v-if="modalProps.isDivider === true"></div>
+            <div
+              class="modal-divider"
+              v-if="modalProps.isDivider === true"
+            ></div>
           </div>
           <div class="modal-main">
             <slot></slot>
@@ -35,95 +42,95 @@
 </template>
 
 <script>
-import { XIcon } from "vue-feather-icons"
-import basicButton from "../common/basicButton.vue"
+  import { XIcon } from "vue-feather-icons"
+  import basicButton from "../common/basicButton.vue"
 
-export default {
-  props: ["modalProps"],
-  components: {
-    XIcon,
-    basicButton
-  },
-  computed: {
-    modalDisplay() {
-      return this.$store.state.orderListModal.modalDisplay
-    }
-  },
-  methods: {
-    onClose() {
-      this.$store.commit("orderListModal/modalOff")
+  export default {
+    props: ["modalProps"],
+    components: {
+      XIcon,
+      basicButton
+    },
+    // computed: {
+    //   modalDisplay() {
+    //     return this.$store.state.orderListModal.modalDisplay
+    //   }
+    // },
+    methods: {
+      // onClose() {
+      //   this.$store.commit("orderListModal/modalOff")
+      // }
     }
   }
-}
 </script>
 
 <style scoped>
-.bg {
-  position: relative;
-}
-.modal-bg {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-}
-.modal-container {
-  width: 640px;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-.close-icon-box {
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
-  background-color: #000;
-  opacity: 0.4;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 20px;
-  cursor: pointer;
-  float: right;
-}
-.close-icon {
-  color: #fff;
-}
-.modal-wrap {
-  width: 640px;
-  background-color: #fff;
-  border-radius: 4px;
-  padding: 32px 32px 40px 32px;
-  clear: both;
-}
-h1 {
-  color: #212121;
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 2px;
-}
-h2 {
-  color: #212121;
-  font-size: 20px;
-  font-weight: bold;
-  margin-top: 24px;
-}
-h3 {
-  color: #666;
-  font-size: 14px;
-  margin-top: 10px;
-}
-.modal-divider {
-  width: 100%;
-  height: 16px;
-  border-bottom: 1px solid #000;
-}
-.btn-wrap {
-  display: flex;
-  justify-content: center;
-  margin-top: 40px;
-}
+  .bg {
+    position: relative;
+  }
+  .modal-bg {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+  .modal-container {
+    width: 640px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .close-icon-box {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    background-color: #000;
+    opacity: 0.4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    float: right;
+  }
+  .close-icon {
+    color: #fff;
+  }
+  .modal-wrap {
+    width: 640px;
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 32px 32px 40px 32px;
+    clear: both;
+  }
+  h1 {
+    color: #212121;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 2px;
+  }
+  h2 {
+    color: #212121;
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 24px;
+  }
+  h3 {
+    color: #666;
+    font-size: 14px;
+    margin-top: 10px;
+  }
+  .modal-divider {
+    width: 100%;
+    height: 16px;
+    border-bottom: 1px solid #000;
+  }
+  .btn-wrap {
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+  }
 </style>
--- components/orderList/orderList.vue
+++ components/orderList/orderList.vue
@@ -8,14 +8,22 @@
           </td>
           <td class="order-list-body">
             <span class="date">{{ orderListData[idxData].paymentDate }}</span>
-            <span class="payment-idx">({{ orderListData[idxData].paymentIdx }})</span>
+            <span class="payment-idx"
+              >({{ orderListData[idxData].paymentIdx }})</span
+            >
             <p class="goods-name">{{ orderListData[idxData].goodsName }}</p>
           </td>
-          <td class="price">{{ `${orderListData[idxData].paymentPrice.toLocaleString()}원` }}</td>
+          <td class="price">
+            {{ `${orderListData[idxData].paymentPrice.toLocaleString()}원` }}
+          </td>
           <td class="status">
-            <span class="status-content">{{ orderListData[idxData].paymentStatus }}</span>
+            <span class="status-content">{{
+              orderListData[idxData].paymentStatus
+            }}</span>
             <br />
-            <span class="final-date">{{ orderListData[idxData].finalDate }}</span>
+            <span class="final-date">{{
+              orderListData[idxData].finalDate
+            }}</span>
           </td>
         </tr>
         <tr class="tr-bottom">
@@ -28,14 +36,18 @@
                   fontWeight="normal"
                   backgroundColor="#fff"
                   borderRadius="4"
-                  bordercolor="#dfdfdf"
+                  borderColor="#dfdfdf"
                   fontSize="14"
                   width="97"
                   height="28"
-                  @event="onModalOnOff('order')"
+                  @event="onModalOn('order')"
                 ></basic-button>
               </div>
-              <modal :modalProps="modalInfoOrder" v-if="modalOrderOpen === true">
+              <modal
+                :modalProps="modalInfoOrder"
+                v-if="modalOrderOpen === true"
+                @close="onModalOff('order')"
+              >
                 <modal-order :paymentData="orderListData[idxData]" />
               </modal>
               <div class="btn-receipt-position">
@@ -45,14 +57,18 @@
                   fontWeight="normal"
                   backgroundColor="#fff"
                   borderRadius="4"
-                  bordercolor="#dfdfdf"
+                  borderColor="#dfdfdf"
                   fontSize="14"
                   width="113"
                   height="28"
-                  @event="onModalOnOff('receipt')"
+                  @event="onModalOn('receipt')"
                 ></basic-button>
               </div>
-              <modal :modalProps="modalInfoReceipt" v-if="modalReceiptOpen === true">
+              <modal
+                :modalProps="modalInfoReceipt"
+                v-if="modalReceiptOpen === true"
+                @close="onModalOff('receipt')"
+              >
                 <modal-receipt :paymentData="orderListData[idxData]" />
               </modal>
             </div>
@@ -64,17 +80,21 @@
               fontWeight="normal"
               backgroundColor="#fff"
               borderRadius="4"
-              bordercolor="#dfdfdf"
+              borderColor="#dfdfdf"
               fontSize="14"
               width="102"
               height="28"
-              @event="onModalOnOff('cancel')"
+              @event="onModalOn('cancel')"
               v-if="
                 orderListData[idxData].cancel === false &&
                 orderListData[idxData].refund === false
               "
             ></basic-button>
-            <modal :modalProps="modalInfoCancel" v-if="modalCancelOpen === true">
+            <modal
+              :modalProps="modalInfoCancel"
+              v-if="modalCancelOpen === true"
+              @close="onModalOff('cancel')"
+            >
               <modal-cancel :paymentData="orderListData[idxData]" />
             </modal>
 
@@ -84,17 +104,21 @@
               fontWeight="normal"
               backgroundColor="#fff"
               borderRadius="4"
-              bordercolor="#dfdfdf"
+              borderColor="#dfdfdf"
               fontSize="14"
               width="102"
               height="28"
-              @event="onModalOnOff('qna')"
+              @event="onModalOn('qna')"
               v-if="
                 orderListData[idxData].cancel === true ||
                 orderListData[idxData].refund === true
               "
             ></basic-button>
-            <modal :modalProps="modalInfoQna" v-if="modalQnaOpen === true">
+            <modal
+              :modalProps="modalInfoQna"
+              v-if="modalQnaOpen === true"
+              @close="onModalOff('qna')"
+            >
               <modal-qna :paymentData="orderListData[idxData]" />
             </modal>
           </td>
@@ -105,164 +129,166 @@
 </template>
 
 <script>
-import basicButton from "../../components/common/basicButton.vue"
-import modal from "../../components/modal/modal.vue"
-import modalOrder from "../../components/modal/modalOrder.vue"
-import modalReceipt from "../../components/modal/modalReceipt.vue"
-import modalCancel from "../../components/modal/modalCancel.vue"
-import modalQna from "../../components/modal/modalQna.vue"
-import {
-  modalInfoCancel,
-  modalInfoOrder,
-  modalInfoReceipt,
-  modalInfoQna
-} from "../../components/modal/modal.js"
+  import basicButton from "../../components/common/basicButton.vue"
+  import modal from "../../components/modal/modal.vue"
+  import modalOrder from "../../components/modal/modalOrder.vue"
+  import modalReceipt from "../../components/modal/modalReceipt.vue"
+  import modalCancel from "../../components/modal/modalCancel.vue"
+  import modalQna from "../../components/modal/modalQna.vue"
+  import {
+    modalInfoCancel,
+    modalInfoOrder,
+    modalInfoReceipt,
+    modalInfoQna
+  } from "../../components/modal/modal.js"
 
-export default {
-  props: ["orderListData", "idxData"],
-  data() {
-    return {
-      modalInfoCancel,
-      modalInfoOrder,
-      modalInfoReceipt,
-      modalInfoQna
-    }
-  },
-  components: {
-    basicButton,
-    modal,
-    modalOrder,
-    modalReceipt,
-    modalCancel,
-    modalQna
-  },
-  computed: {
-    modalOrderOpen() {
-      return this.$store.state.orderListModal.modalOrderOpen
+  export default {
+    props: ["orderListData", "idxData"],
+    data() {
+      return {
+        modalInfoCancel,
+        modalInfoOrder,
+        modalInfoReceipt,
+        modalInfoQna
+      }
     },
-    modalReceiptOpen() {
-      return this.$store.state.orderListModal.modalReceiptOpen
+    components: {
+      basicButton,
+      modal,
+      modalOrder,
+      modalReceipt,
+      modalCancel,
+      modalQna
     },
-    modalCancelOpen() {
-      return this.$store.state.orderListModal.modalCancelOpen
+    computed: {
+      modalOrderOpen() {
+        return this.$store.state.orderListModal.modalOrderOpen
+      },
+      modalReceiptOpen() {
+        return this.$store.state.orderListModal.modalReceiptOpen
+      },
+      modalCancelOpen() {
+        return this.$store.state.orderListModal.modalCancelOpen
+      },
+      modalQnaOpen() {
+        return this.$store.state.orderListModal.modalQnaOpen
+      }
     },
-    modalQnaOpen() {
-      return this.$store.state.orderListModal.modalQnaOpen
-    }
-  },
-  methods: {
-    /** 모달 팝업 열고 닫는 기능 함수 */
-    onModalOnOff(type) {
-      this.$store.commit("orderListModal/modalOn", type)
-      console.log(type)
+    methods: {
+      /** 모달 팝업 열고 닫는 기능 함수 */
+      onModalOn(type) {
+        this.$store.commit("orderListModal/modalOn", type)
+      },
+      onModalOff(type) {
+        this.$store.commit("orderListModal/modalOff", type)
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.order-list {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-table {
-  border-collapse: collapse;
-}
-table:hover {
-  background-color: rgba(255, 255, 255, 0);
-}
-.tr-main {
-  height: 108px;
-  border-bottom: 1px solid #ececec;
-}
-img {
-  width: 103px;
-  height: 76px;
-  border-radius: 4px;
-}
-/** 테이블 각 td 클래스별 서식 */
-.order-list-img {
-  width: 151px;
-  line-height: 0;
-  display: table-cell;
-  vertical-align: middle;
-  text-align: center;
-}
-.order-list-body {
-  width: 413px;
-  display: table-cell;
-  vertical-align: middle;
-}
-.price {
-  width: 140px;
-  border-left: 1px solid #ececec;
-  border-right: 1px solid #ececec;
-  display: table-cell;
-  vertical-align: middle;
-  text-align: center;
-  font-size: 14px;
-  color: #212121;
-}
-.status {
-  width: 140px;
-  display: table-cell;
-  vertical-align: middle;
-  text-align: center;
-}
-.tr-bottom {
-  height: 56px;
-  border: 1px solid #ececec;
-}
-/** 각 td 안 내용 서식 */
-.date {
-  font-size: 14px;
-  color: #212121;
-}
-.payment-idx {
-  font-size: 14px;
-  color: #666;
-}
-.body-content {
-  margin: 0;
-  padding: 0;
-}
-.goods-name {
-  margin-top: 8px;
-  width: 268px;
-  height: 48px;
-  font-size: 16px;
-  color: #212121;
-  /* 넘치는 글자 말줄임표로 처리 */
-  text-overflow: ellipsis;
-  white-space: normal;
-  overflow: hidden;
-  word-wrap: break-word;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-}
-.status-content {
-  font-size: 14px;
-  font-weight: bold;
-  color: #212121;
-}
-.final-date {
-  color: #8f8f8f;
-  font-size: 14px;
-}
-.bottom-left-wrap {
-  display: flex;
-  padding: 14px 32px;
-}
-.bottom-right {
-  padding: 14px 24px;
-}
-.btn-order-position {
-  margin: 0;
-  padding: 0;
-}
-.btn-receipt-position {
-  margin-left: 8px;
-}
+  .order-list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  table:hover {
+    background-color: rgba(255, 255, 255, 0);
+  }
+  .tr-main {
+    height: 108px;
+    border-bottom: 1px solid #ececec;
+  }
+  img {
+    width: 103px;
+    height: 76px;
+    border-radius: 4px;
+  }
+  /** 테이블 각 td 클래스별 서식 */
+  .order-list-img {
+    width: 151px;
+    line-height: 0;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+  }
+  .order-list-body {
+    width: 413px;
+    display: table-cell;
+    vertical-align: middle;
+  }
+  .price {
+    width: 140px;
+    border-left: 1px solid #ececec;
+    border-right: 1px solid #ececec;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    font-size: 14px;
+    color: #212121;
+  }
+  .status {
+    width: 140px;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+  }
+  .tr-bottom {
+    height: 56px;
+    border: 1px solid #ececec;
+  }
+  /** 각 td 안 내용 서식 */
+  .date {
+    font-size: 14px;
+    color: #212121;
+  }
+  .payment-idx {
+    font-size: 14px;
+    color: #666;
+  }
+  .body-content {
+    margin: 0;
+    padding: 0;
+  }
+  .goods-name {
+    margin-top: 8px;
+    width: 268px;
+    height: 48px;
+    font-size: 16px;
+    color: #212121;
+    /* 넘치는 글자 말줄임표로 처리 */
+    text-overflow: ellipsis;
+    white-space: normal;
+    overflow: hidden;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+  .status-content {
+    font-size: 14px;
+    font-weight: bold;
+    color: #212121;
+  }
+  .final-date {
+    color: #8f8f8f;
+    font-size: 14px;
+  }
+  .bottom-left-wrap {
+    display: flex;
+    padding: 14px 32px;
+  }
+  .bottom-right {
+    padding: 14px 24px;
+  }
+  .btn-order-position {
+    margin: 0;
+    padding: 0;
+  }
+  .btn-receipt-position {
+    margin-left: 8px;
+  }
 </style>
