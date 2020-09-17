5cb7b2d2ab10675fc814aa4c170350b6efbd4842
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-18 00:24:39 +0900

    Add: sidebarMenu 컴포넌트 추가

--- pages/mypage/notification/index.vue
+++ pages/mypage/notification/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="notification-page">
-    <div class="my-shopping-cast">
-      <my-shopping-cast
-        title="알림"
-        :tableColumnWidth="tableColumnWidth"
-        :tableHeader="notificationHeaderList"
-        :tableList="notificationLists"
-      />
+    <div class="sidebar">
+      <sidebar-menu />
+    </div>
+    <div>
+      <div class="my-shopping-cast">
+        <my-shopping-cast
+          title="알림"
+          :tableColumnWidth="tableColumnWidth"
+          :tableHeader="notificationHeaderList"
+          :tableList="notificationLists"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +20,10 @@
   import myShoppingCast from "../../../components/common/myShoppingCast.vue"
   // 알림 list data
   import { notificationLists } from "../../../components/myPage/notificationLists.js"
+  // 사이드바 메뉴 컴포넌트
+  import sidebarMenu from "../../../components/common/sidebarMenu.vue"
+  // 로그아웃 상태 컴포넌트
+  import status from "../../../components/common/status.vue"
 
   export default {
     data() {
@@ -32,15 +41,23 @@
     },
     components: {
       "my-shopping-cast": myShoppingCast,
-      "notification-lists": notificationLists
+      "notification-lists": notificationLists,
+
+      // status: status, ""가 자꾸 삭제돼요!!!!!
+      "sidebar-menu": sidebarMenu
     }
   }
 </script>
 <style scoped>
-  .notification-page .notification {
-    display: block;
+  .notification-page {
+    display: flex;
+    margin: 0 auto;
+  }
+  .notification-page .sidebar {
+    display: inline-block;
   }
   .notification-page .my-shopping-cast {
+    display: inline-block;
     margin: 32px 0 0 16px;
   }
 </style>
