a9a3c706994d5a1878f391c8fae516a5c9a7f3d6
dongmin <ehdals0227@gmail.com>
2020-09-17 17:48:45 +0900

    Fix: 알림리스트에 delete값 추가

--- components/myPage/notificationLists.js
+++ components/myPage/notificationLists.js
@@ -3,30 +3,35 @@ export const notificationLists = [
     id: 1,
     date: "2020-03-13  10:30",
     title: "[결제확인] 주문번호 23451 결제완료",
-    sender: "고객센터"
+    sender: "고객센터",
+    delete: true
   },
   {
     id: 2,
     date: "2020-03-13  10:30",
     title: "[결제확인] 주문번호 23452 결제완료",
-    sender: "고객센터"
+    sender: "고객센터",
+    delete: true
   },
   {
     id: 3,
     date: "2020-03-13  10:30",
     title: "[결제확인] 주문번호 23453 결제완료",
-    sender: "고객센터"
+    sender: "고객센터",
+    delete: true
   },
   {
     id: 4,
     date: "2020-03-13  10:30",
     title: "[결제확인] 주문번호 23454 결제완료",
-    sender: "고객센터"
+    sender: "고객센터",
+    delete: true
   },
   {
     id: 5,
     date: "2020-03-13  10:30",
     title: "[결제확인] 주문번호 23455 결제완료",
-    sender: "고객센터"
+    sender: "고객센터",
+    delete: true
   }
 ]
