8a81827efef3cad8a2acb3aec26cd3f191101e90
dongmin <ehdals0227@gmail.com>
2020-09-17 17:51:57 +0900

    Fix: notificationList 관련 데이터 추가 후 props로 전달

--- pages/mypage/notification/index.vue
+++ pages/mypage/notification/index.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="notification-page">
     <div class="my-shopping-cast">
-      <my-shopping-cast />
-    </div>
-    <div class="notification">
-      <!-- <notification-content /> -->
+      <my-shopping-cast
+        title="알림"
+        :tableColumnWidth="tableColumnWidth"
+        :tableHeader="notificationHeaderList"
+        :tableList="notificationLists"
+      />
     </div>
   </div>
 </template>
 <script>
-  import myShoppingCast from "../../../components/common/myShoppingCast.vue"
   // 나의 쇼핑 메뉴 테이블 공통 캄포넌트
-  import notificationContents from "../../../components/myPage/notificationContents"
-  // 나의 쇼핑 메뉴 중 알림 테이블 콘텐츠 컴포넌트
-  import { notificationLists } from "../../../components/myPage/notificationLists.js"
+  import myShoppingCast from "../../../components/common/myShoppingCast.vue"
   // 알림 list data
+  import { notificationLists } from "../../../components/myPage/notificationLists.js"
+
   export default {
     data() {
       return {
+        tableColumnWidth: [60, 140, 364, 140, 140],
+        notificationHeaderList: [
+          "No.",
+          "수신일시",
+          "제목/내용",
+          "발신자",
+          "삭제"
+        ],
         notificationLists
       }
     },
     components: {
-      "notification-content": notificationContents,
       "my-shopping-cast": myShoppingCast,
       "notification-lists": notificationLists
     }
