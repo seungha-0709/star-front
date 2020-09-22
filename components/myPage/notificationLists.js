5ab219f44cf26c3d99f8a9c9b09a40f7d8992ce4
Sang Yun Lee <sangyun202@gmail.com>
2020-09-22 09:50:00 +0900

    Notice: 데이터 형태 확인 요청

--- components/myPage/notificationLists.js
+++ components/myPage/notificationLists.js
@@ -1,37 +1,80 @@
-export const notificationLists = [
-  {
-    id: 1,
-    date: "2020-03-13  10:30",
-    title: "[결제확인] 주문번호 23451 결제완료",
-    sender: "고객센터",
-    delete: true
-  },
-  {
-    id: 2,
-    date: "2020-03-13  10:30",
-    title: "[결제확인] 주문번호 23452 결제완료",
-    sender: "고객센터",
-    delete: true
-  },
-  {
-    id: 3,
-    date: "2020-03-13  10:30",
-    title: "[결제확인] 주문번호 23453 결제완료",
-    sender: "고객센터",
-    delete: true
-  },
-  {
-    id: 4,
-    date: "2020-03-13  10:30",
-    title: "[결제확인] 주문번호 23454 결제완료",
-    sender: "고객센터",
-    delete: true
-  },
-  {
-    id: 5,
-    date: "2020-03-13  10:30",
-    title: "[결제확인] 주문번호 23455 결제완료",
-    sender: "고객센터",
-    delete: true
-  }
-]
+export const notificationLists = {
+  total: 5,
+  page: 1,
+  limit: 10,
+  data: [
+    {
+      id: 1,
+      date: "2020-03-13  10:30",
+      title: "[결제확인] 주문번호 23451 결제완료",
+      sender: "고객센터",
+      delete: true
+    },
+    {
+      id: 2,
+      date: "2020-03-13  10:30",
+      title: "[결제확인] 주문번호 23452 결제완료",
+      sender: "고객센터",
+      delete: true
+    },
+    {
+      id: 3,
+      date: "2020-03-13  10:30",
+      title: "[결제확인] 주문번호 23453 결제완료",
+      sender: "고객센터",
+      delete: true
+    },
+    {
+      id: 4,
+      date: "2020-03-13  10:30",
+      title: "[결제확인] 주문번호 23454 결제완료",
+      sender: "고객센터",
+      delete: true
+    },
+    {
+      id: 5,
+      date: "2020-03-13  10:30",
+      title: "[결제확인] 주문번호 23455 결제완료",
+      sender: "고객센터",
+      delete: true
+    }
+  ]
+}
+
+// export const notificationLists = [
+//   {
+//     id: 1,
+//     date: "2020-03-13  10:30",
+//     title: "[결제확인] 주문번호 23451 결제완료",
+//     sender: "고객센터",
+//     delete: true
+//   },
+//   {
+//     id: 2,
+//     date: "2020-03-13  10:30",
+//     title: "[결제확인] 주문번호 23452 결제완료",
+//     sender: "고객센터",
+//     delete: true
+//   },
+//   {
+//     id: 3,
+//     date: "2020-03-13  10:30",
+//     title: "[결제확인] 주문번호 23453 결제완료",
+//     sender: "고객센터",
+//     delete: true
+//   },
+//   {
+//     id: 4,
+//     date: "2020-03-13  10:30",
+//     title: "[결제확인] 주문번호 23454 결제완료",
+//     sender: "고객센터",
+//     delete: true
+//   },
+//   {
+//     id: 5,
+//     date: "2020-03-13  10:30",
+//     title: "[결제확인] 주문번호 23455 결제완료",
+//     sender: "고객센터",
+//     delete: true
+//   }
+// ]
