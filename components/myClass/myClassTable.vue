a3bd129b15c62b0deca6fdf63dc38f66b8468674
Seungha <sh.kim.x928@gmail.com>
2020-09-23 02:54:02 +0900

    Add: 내 강의 보기 컴포넌트 추가

---
+++ components/myClass/myClassTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="(myClass, index) in boardData.data" :key="index">
+          <td :style="{ width: myPageBoardInfo.tableColumnWidth[0] + 'px' }">
+            1
+          </td>
+          <td :style="{ width: myPageBoardInfo.tableColumnWidth[1] + 'px' }">
+            {{ myClass.date }}
+          </td>
+          <td :style="{ width: myPageBoardInfo.tableColumnWidth[2] + 'px' }">
+            {{ myClass.name }}
+          </td>
+          <td :style="{ width: myPageBoardInfo.tableColumnWidth[3] + 'px' }">
+            {{ myClass.statusTitle }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["myPageBoardInfo", "boardData"]
+  }
+</script>
---
+++ pages/myclass/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <my-page-board :myPageBoardInfo="myClassInfo">
+      <my-class-table :myPageBoardInfo="myClassInfo" :boardData="myClassList" />
+    </my-page-board>
+  </div>
+</template>
+
+<script>
+  //   import { mapState } from "vuex"
+  import myPageBoard from "../../components/myClass/myPageBoard.vue"
+  import myClassTable from "../../components/myClass/myClassTable.vue"
+  import { myClassList } from "../../components/myClass/myClassList.js"
+  import { myClassInfo } from "../../components/myPageBoard.js"
+
+  export default {
+    data() {
+      return {
+        myClassList,
+        myClassInfo
+      }
+    },
+    components: {
+      myPageBoard,
+      myClassTable
+    },
+    computed: {
+      //   myClassInfo() {
+      //     return this.$store.state.myPageBoard.myClassInfo
+      //   }
+      //   ...mapState("myPageBoard", ["myClassInfo"])
+    }
+  }
+</script>
