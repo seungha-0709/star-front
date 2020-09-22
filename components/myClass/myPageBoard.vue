5c2ac7a90c6a6e883ba956b98050fee155bea80d
Seungha <sh.kim.x928@gmail.com>
2020-09-23 02:17:38 +0900

    Fix: 거래내역 마이페이지 컴포넌트에 삽입

---
+++ components/myClass/myPageBoard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="basic-board">
+    <div>
+      <h3 class="basic-board-menu">{{ myPageBoardInfo.title }}</h3>
+    </div>
+    <div v-if="myPageBoardInfo.subTitle">{{ myPageBoardInfo.subTitle }}</div>
+    <div class="content-header-wrap">
+      <p class="content-count" v-if="myPageBoardInfo.isAllAmount">
+        {{ `총 ${myPageBoard.allAmount}개` }}
+      </p>
+      <p class="content-count" v-if="myPageBoardInfo.isAllAmountOrder">
+        {{ `전체 주문내역 ${myPageBoardInfo.allAmount}건` }}
+      </p>
+      <basic-button
+        v-if="myPageBoardInfo.removeAllBtn"
+        text="모두삭제"
+        width="84"
+        height="42"
+        color="#212121"
+        fontSize="14"
+        borderRadius="4"
+        backgroundColor="#ffffff"
+        borderColor="#dfdfdf"
+        fontWeight="400"
+        :event="deleteAll"
+      />
+    </div>
+    <table class="basic-board-table">
+      <thead>
+        <tr class="table-header">
+          <th
+            v-for="(title, index) in myPageBoardInfo.thTitle"
+            :key="index"
+            :style="{ width: myPageBoardInfo.tableColumnWidth[index] + 'px' }"
+          >
+            {{ title }}
+          </th>
+        </tr>
+      </thead>
+    </table>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import basicButton from "../common/basicButton.vue"
+
+  export default {
+    props: ["myPageBoardInfo", "boardData"],
+    components: {
+      basicButton
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+  .basic-board {
+    width: 924px;
+    border: solid 1px #dfdfdf;
+    background-color: #ffffff;
+    padding: 32px 40px 40px 40px;
+    font-size: 14px;
+    font-weight: normal;
+    color: #212121;
+    vertical-align: middle;
+  }
+  .basic-board-menu {
+    text-align: left;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 2px;
+  }
+  .content-header-wrap {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 14px;
+  }
+  .content-count {
+    margin-top: 22px;
+  }
+</style>
---
+++ pages/mypage/orderList/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="my-page-wrap">
+    <div class="sidebar">
+      <sidebar-temporal />
+    </div>
+    <div class="my-page-content">
+      <my-page-board
+        :myPageBoardInfo="myOrderListInfo"
+        :boardData="myClassList"
+      >
+        <order-list-main />
+      </my-page-board>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sidebarTemporal from "../../../components/myPage/sidebarTemporal"
+  import myPageBoard from "../../../components/myClass/myPageBoard.vue"
+  import orderListMain from "../../../components/orderList/orderListMain.vue"
+  import { myClassList } from "../../../components/myClass/myClassList.js"
+  import { myOrderListInfo } from "../../../components/myPageBoard.js"
+
+  export default {
+    data() {
+      return {
+        myClassList,
+        myOrderListInfo
+      }
+    },
+    components: {
+      sidebarTemporal,
+      myPageBoard,
+      orderListMain
+    }
+  }
+</script>
+
+<style scoped>
+  .my-page-wrap {
+    width: 1200px;
+    display: flex;
+    margin: 0 auto;
+  }
+  .sidebar {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+  }
+  .my-page-content {
+    display: inline-block;
+  }
+</style>
