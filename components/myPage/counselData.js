f198ad63199b4d51b9d0fd45416dc67c470faf10
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-23 23:17:05 +0900

    Add: counsel 임시 데이터 .js 파일로 등록

---
+++ components/myPage/counselData.js
@@ -0,0 +1,37 @@
+export const counselData = {
+  total: 5,
+  page: 1,
+  limit: 10,
+  data: [
+    {
+      date: "배송",
+      title: "못받았는데 다시 보내줄수있나요?",
+      sender: "2020-03-20  10:30",
+      delete: "답변대기"
+    },
+    {
+      date: "배송",
+      title: "못받았는데 다시 보내줄수있나요?",
+      sender: "2020-03-20  10:30",
+      delete: "답변대기"
+    },
+    {
+      date: "배송",
+      title: "못받았는데 다시 보내줄수있나요?",
+      sender: "2020-03-20  10:30",
+      delete: "답변대기"
+    },
+    {
+      date: "배송",
+      title: "못받았는데 다시 보내줄수있나요?",
+      sender: "2020-03-20  10:30",
+      delete: "답변대기"
+    },
+    {
+      date: "배송",
+      title: "못받았는데 다시 보내줄수있나요?",
+      sender: "2020-03-20  10:30",
+      delete: "답변대기"
+    }
+  ]
+}
---
+++ components/myPage/counselList.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- basicBoardCast, Component 수정사항
+Title 하단 설명(쇼핑몰 이용에 궁금하신 사항을 남겨주세요.) 추가
+선택삭제, 모두삭제 버튼 삭제
+페이지네이션 우측 글쓰기 버튼 추가 -->
+</template>
