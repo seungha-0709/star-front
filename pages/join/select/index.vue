394ca12a1434feb30379e589d4632c0e594dc721
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-26 21:38:33 +0900

    Edit: joinSelectCard 컴포넌트를 pages index에 추가, 스타일 수정

--- pages/join/select/index.vue
+++ pages/join/select/index.vue
@@ -6,13 +6,18 @@
         description="본인이 해당하시는 가입 유형을 아래에서 선택해주세요."
       />
     </div>
+    <div class="join-select-group">
+      <join-select />
+    </div>
   </div>
 </template>
 <script>
   import basicJoinTitle from "../../../components/common/join/basicJoinTitle"
+  import joinSelect from "../../../components/join/joinSelect"
   export default {
     components: {
-      "basic-join-title": basicJoinTitle
+      "basic-join-title": basicJoinTitle,
+      "join-select": joinSelect
     }
   }
 </script>
@@ -22,5 +27,8 @@
   }
   .join-select-page .join-select-title {
     margin-top: 85px;
+  }
+  .join-select-page .join-select-group {
+    margin-top: 53px;
   }
 </style>
