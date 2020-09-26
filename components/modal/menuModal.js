4bbf54172b7b0a84cd9f416320628ed2c7ab6d77
Seungha <sh.kim.x928@gmail.com>
2020-09-27 00:22:33 +0900

    Add: 모바일 화면에서 나타나는 탭 이동 메뉴 화면 추가

---
+++ components/modal/menuModal.js
@@ -0,0 +1,7 @@
+export const policyMenuModal = [
+  "이용약관",
+  "개인정보처리방침",
+  "정보제공활용동의",
+  "이메일 집단수집거부",
+  "랭킹산정 Logic"
+]
--- pages/policy/index.vue
+++ pages/policy/index.vue
@@ -1,29 +1,43 @@
 <template>
-  <div class="policy">
-    <div class="tab-menu-wrap">
-      <ul class="tab-menu">
-        <li
-          v-for="(data, index) in tabData"
-          :key="index"
-          v-on:click="onTypeChange(data.type)"
-          :class="{ active: data.type === activeType }"
-        >
-          {{ data.name }}
-        </li>
-      </ul>
-    </div>
-    <div class="policy-content">
-      <tab v-bind:tab-type="activeType"></tab>
+  <div>
+    <div class="policy">
+      <div class="tab-menu-mobile" @click="onMenuOnOff">
+        {{ tabData.find((tabdata) => tabdata.type === activeType).name }}
+      </div>
+      <div class="tab-menu-wrap">
+        <ul class="tab-menu">
+          <li
+            v-for="(data, index) in tabData"
+            :key="index"
+            @click="onTypeChange(data.type)"
+            :class="{ active: data.type === activeType }"
+          >
+            {{ data.name }}
+          </li>
+        </ul>
+      </div>
+      <div class="policy-content">
+        <tab :tab-type="activeType"></tab>
+      </div>
+      <menu-modal
+        :modalMenuList="tabData.name"
+        :tabChange="tabData"
+        :menuOnOff="isMenuOnoff"
+        @tab="onTypeChange"
+        @menuClose="onMenuOnOff"
+      />
     </div>
   </div>
 </template>
 
 <script>
   import TabVue from "../../components/policyTab.vue"
+  import menuModal from "../../components/modal/menuModal.vue"
 
   export default {
     data() {
       return {
+        isMenuOnoff: false,
         tabData: [
           {
             type: "service",
@@ -50,11 +64,16 @@
       }
     },
     components: {
-      tab: TabVue
+      tab: TabVue,
+      menuModal
     },
     methods: {
       onTypeChange(type) {
         this.activeType = type
+      },
+      onMenuOnOff() {
+        console.log(this.isMenuOnoff)
+        this.isMenuOnoff = !this.isMenuOnoff
       }
     }
   }
@@ -99,5 +118,30 @@
     padding: 28px 32px;
     text-align: left;
     border: 1px solid #dfdfdf;
+  }
+  .tab-menu-mobile {
+    background-color: rgb(251, 251, 251);
+    border: 1px solid #dfdfdf;
+    width: 100%;
+    height: 42px;
+    align-items: center;
+    padding: 0 16px;
+    font-size: 12px;
+    color: #212121;
+    display: none;
+  }
+  @media (max-width: 1199px) {
+    .tab-menu-wrap {
+      display: none;
+    }
+    .tab-menu-mobile {
+      display: flex;
+    }
+    .policy {
+      width: 100%;
+    }
+    .policy-content {
+      margin-top: 8px;
+    }
   }
 </style>
