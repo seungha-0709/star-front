3e7782d56dcd820adef7282cb9c6fa0a9710e718
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-26 21:37:42 +0900

    Add: joinSelectCard 모음 component,  joinSelect 생성

---
+++ components/join/joinSelect.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="join-select-cards">
+    <div class="join-select-card">
+      <basic-join-select-card
+        type="학생"
+        description="리뷰작성과 함께 모든 선생님들의 리뷰를
+        확인하실 수 있습니다."
+      >
+        <img
+          src="https://cdn.zeplin.io/5c9896e0ea78dc066bbc8cac/assets/68A7DF9F-3BFD-4B8F-A8D9-BA8D3808DA4F.png"
+          alt="student image"
+          width="120px"
+          height="85px"
+        />
+      </basic-join-select-card>
+    </div>
+    <div class="join-select-card">
+      <basic-join-select-card
+        type="학부모"
+        description="리뷰작성과 함께 우리아이의 선생님과 
+유치원, 어린이집의 리뷰를 보실 수 
+있습니다."
+      >
+        <img
+          src="https://cdn.zeplin.io/5c9896e0ea78dc066bbc8cac/assets/789034D3-E670-4A32-9912-E5AEBD1FFA55.png"
+          alt="parents image"
+          width="120px"
+          height="85px"
+        />
+      </basic-join-select-card>
+    </div>
+    <div class="join-select-card">
+      <basic-join-select-card
+        type="선생님 / 교수님"
+        description="세상 가장 쉬운 방법으로 우리 학생들과 소통을 할 수 도, 내 강의를 등록할 수도 
+있습니다."
+      >
+        <img
+          src="https://cdn.zeplin.io/5c9896e0ea78dc066bbc8cac/assets/32B16243-3355-4A68-A261-B19C7BD3A98E.png"
+          alt="teacher image"
+          width="120px"
+          height="85px"
+        />
+      </basic-join-select-card>
+    </div>
+    <div class="join-select-card-last">
+      <basic-join-select-card
+        type="학원/유치원 등 기업회원"
+        description="소속 강사, 학생관리 서비스를 이용하실 수 있습니다.
+  "
+      >
+        <img
+          src="https://cdn.zeplin.io/5c9896e0ea78dc066bbc8cac/assets/7E896F2D-A9B4-4961-8058-114D5CE667DD.png"
+          alt="parents image"
+          width="120px"
+          height="85px"
+        />
+      </basic-join-select-card>
+    </div>
+  </div>
+</template>
+<script>
+  import basicJoinSelectCard from "../common/join/basicJoinSelectCard.vue"
+  export default {
+    components: {
+      "basic-join-select-card": basicJoinSelectCard
+    }
+  }
+</script>
+<style scoped>
+  .join-select-cards {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  }
+  .join-select-cards .join-select-card {
+    margin-right: 24px;
+  }
+</style>
