e49a680ffe2937462f06f591c19b193d1de91906
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-26 14:34:43 +0900

    Add: basicJoinTitle Component add it to pages

---
+++ components/common/join/basicJoinTitle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="basic-join">
+    <div class="logo">
+      <img
+        src="https://d1ta1myjmiqbpz.cloudfront.net/static/images/logo/logo_square_gray.png"
+        alt="logo"
+      />
+    </div>
+    <div class="join-title">
+      <span class="starteacher">별별선생 </span>
+      <span class="subtitle">{{ subtitle }}</span>
+    </div>
+    <p class="description">{{ description }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      subtitle: {
+        default: ""
+      },
+      description: {
+        default: ""
+      }
+    }
+  }
+</script>
+<style scoped>
+  .basic-join {
+    text-align: center;
+    vertical-align: middle;
+    font-weight: normal;
+  }
+  .basic-join .logo img {
+    width: 42px;
+    height: 41px;
+    margin-bottom: 4px;
+  }
+  .basic-join .join-title {
+    height: 59px;
+    font-size: 40px;
+  }
+  .basic-join .starteacher {
+    color: #3f60cc;
+    font-weight: bold;
+  }
+  .basic-join .subtitle {
+    color: #212121;
+  }
+  .basic-join .description {
+    height: 54px;
+    font-size: 20px;
+    color: #666666;
+    padding-top: 17px;
+  }
+</style>
---
+++ pages/join/select/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="join-select-page">
+    <div class="join-select-title">
+      <basic-join-title
+        subtitle="회원가입"
+        description="본인이 해당하시는 가입 유형을 아래에서 선택해주세요."
+      />
+    </div>
+  </div>
+</template>
+<script>
+  import basicJoinTitle from "../../../components/common/join/basicJoinTitle"
+  export default {
+    components: {
+      "basic-join-title": basicJoinTitle
+    }
+  }
+</script>
+<style scoped>
+  .join-select-page {
+    margin: 0 auto;
+  }
+  .join-select-page .join-select-title {
+    margin-top: 85px;
+  }
+</style>
