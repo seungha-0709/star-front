32a1d52a19f1a19d5edc7ad58d75faa44896c378
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-27 13:55:45 +0900

    Edit: content only layout add and style edit

--- pages/join/select/index.vue
+++ pages/join/select/index.vue
@@ -9,26 +9,49 @@
     <div class="join-select-group">
       <join-select />
     </div>
+    <div class="sign-in">
+      <p>이미 계정이 있으신가요?</p>
+      <button>로그인</button>
+    </div>
+    <copy-right />
   </div>
 </template>
 <script>
+  import copyright from "../../../components/common/copyright"
   import basicJoinTitle from "../../../components/common/join/basicJoinTitle"
   import joinSelect from "../../../components/join/joinSelect"
   export default {
+    layout: "contentOnly",
     components: {
       "basic-join-title": basicJoinTitle,
-      "join-select": joinSelect
+      "join-select": joinSelect,
+      "copy-right": copyright
     }
   }
 </script>
 <style scoped>
   .join-select-page {
     margin: 0 auto;
+    height: 100%;
   }
   .join-select-page .join-select-title {
     margin-top: 85px;
   }
   .join-select-page .join-select-group {
     margin-top: 53px;
+  }
+  .join-select-page .sign-in {
+    display: flex;
+    justify-content: center;
+    font-size: 16px;
+    font-weight: normal;
+    margin: 40px 0 96px;
+  }
+  .join-select-page .sign-in p {
+    color: #212121;
+    margin-right: 8px;
+  }
+  .join-select-page .sign-in button {
+    color: #3f60cc;
   }
 </style>
