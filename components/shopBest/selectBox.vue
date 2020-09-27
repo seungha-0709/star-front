1f2b33849ccb87ca911456d7791599a16f115ef4
Seungha <sh.kim.x928@gmail.com>
2020-09-27 11:37:40 +0900

    Fix: selectbox 재사용 가능하게 수정

--- components/shopBest/selectBox.vue
+++ components/shopBest/selectBox.vue
@@ -5,7 +5,7 @@
       <div
         class="option"
         @click="onSortChange(select.sort)"
-        v-for="(select, i) in selectCreteria"
+        v-for="(select, i) in sortList"
         :key="i"
       >
         <input type="radio" class="radio" name="category" />
@@ -21,21 +21,11 @@
 
 <script>
   import { ChevronDownIcon } from "vue-feather-icons"
-  import { shopBestList } from "./shopBestList.js"
 
   export default {
-    props: ["itemList"],
+    props: ["sortList"],
     data() {
-      return {
-        shopBestList,
-        selectCreteria: [
-          /* 셀렉트 박스에 들어가는 옵션항목 */
-          { title: "최신순", sort: "latest_at" },
-          { title: "가격 낮은 순", sort: "low_price" },
-          { title: "가격 높은 순", sort: "high_price" },
-          { title: "좋아요 많은 순", sort: "high_like" }
-        ]
-      }
+      return {}
     },
     components: {
       ChevronDownIcon
@@ -54,7 +44,7 @@
       const dropDown = document.querySelector(".drop-down-icon")
       const selectedOption = document.querySelector(".selected-option")
 
-      selectedOption.innerHTML = this.selectCreteria[0].title
+      selectedOption.innerHTML = this.sortList[0].title
 
       selected.addEventListener("click", () => {
         optionsContainer.classList.toggle("active")
--- pages/store/best.vue
+++ pages/store/best.vue
@@ -5,7 +5,7 @@
       <div class="shop-best-item-top">
         <span class="shop-best-item-sub-title">{{ `${total}개의 강의` }}</span>
         <div class="select-box">
-          <selectBox @listAlign="onAlignChange" />
+          <selectBox @listAlign="onAlignChange" :sortList="selectCreteria" />
         </div>
       </div>
       <div class="shop-best-item-wrap">
@@ -29,7 +29,14 @@
     data() {
       return {
         shopBestList,
-        total: 9
+        total: 9,
+        selectCreteria: [
+          /* 셀렉트 박스에 들어가는 옵션항목 */
+          { title: "최신순", sort: "latest_at" },
+          { title: "가격 낮은 순", sort: "low_price" },
+          { title: "가격 높은 순", sort: "high_price" },
+          { title: "좋아요 많은 순", sort: "high_like" }
+        ]
       }
     },
     computed: {
