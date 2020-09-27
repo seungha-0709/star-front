e5bc930d0f79fb10044fc668e0e133fc77226ad5
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-27 13:54:51 +0900

    Add: copyright component

---
+++ components/common/copyright.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="copyright">Copyright © TEvalueWise Corp. All Rights Reserved.</p>
+</template>
+<script>
+  export default {}
+</script>
+<style scoped>
+  .copyright {
+    font-size: 14px;
+    font-weight: normal;
+    color: #c1c1c1;
+    text-align: center;
+  }
+</style>
