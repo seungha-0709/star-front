ed5d64f1fa1932cee3f56cbf5eac30cf06ae4f37
Seungha <sh.kim.x928@gmail.com>
2020-10-04 16:36:24 +0900

    Add: 회원가입 페이지 데이터 파일 추가

---
+++ assets/data/joinUser.js
@@ -0,0 +1,52 @@
+export const joinUserCardInfo = [
+  {
+    name: "email",
+    type: "이메일로 회원가입",
+    description: "사용하는 이메일로 가입하기!",
+    width: "107",
+    img: {
+      src: "/img/image/join_email.png",
+      alt: "email image",
+      width: "120px",
+      height: "120px"
+    }
+  },
+  {
+    name: "naver",
+    type: "네이버 간편회원가입",
+    description: "사용하는 네이버 계정으로 가입하기!",
+    backgroundColor: "#3ec728",
+    width: "148",
+    img: {
+      src: "/img/image/join_naver.png",
+      alt: "네이버 로고",
+      width: "120px",
+      height: "120px"
+    }
+  },
+  {
+    name: "kakao",
+    type: "카카오톡 간편회원가입",
+    description: "사용하는 카카오톡 계정으로 가입하기!",
+    backgroundColor: "#fee202",
+    color: "#423630",
+    img: {
+      src: "/img/image/join_kakao.png",
+      alt: "카카오톡 로고",
+      width: "120px",
+      height: "120px"
+    }
+  },
+  {
+    name: "facebook",
+    type: "페이스북 간편회원가입",
+    description: "사용하는 페이스북 계정으로 가입하기!",
+    backgroundColor: "#3a5998",
+    img: {
+      src: "/img/image/join_facebook.png",
+      alt: "페이스북 로고",
+      width: "120px",
+      height: "120px"
+    }
+  }
+]
