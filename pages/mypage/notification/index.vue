2f288c5290a41be734e5b8e648a97a27ebe5046c
Seungha <sh.kim.x928@gmail.com>
2020-10-04 16:00:46 +0900

    Fix: myPageBoard.js 경로 변경 내용 index.vue에 반영

--- pages/mypage/notification/index.vue
+++ pages/mypage/notification/index.vue
@@ -13,39 +13,39 @@
   </div>
 </template>
 <script>
-  // 나의 쇼핑 메뉴 테이블 공통 캄포넌트
-  import basicBoardCast from "../../../components/common/basicBoardCast.vue"
-  // 사이드바 메뉴 컴포넌트 입시 import
-  import sidebarTemporal from "../../../components/myPage/sidebarTemporal.vue"
-  // 알림 list data
-  import { notificationLists } from "../../../components/myPage/notificationLists.js"
-  import { myNoticeInfo } from "../../../components/myPageBoard.js"
+// 나의 쇼핑 메뉴 테이블 공통 캄포넌트
+import basicBoardCast from "../../../components/common/basicBoardCast.vue"
+// 사이드바 메뉴 컴포넌트 입시 import
+import sidebarTemporal from "../../../components/myPage/sidebarTemporal.vue"
+// 알림 list data
+import { notificationLists } from "../../../components/myPage/notificationLists.js"
+import { myNoticeInfo } from "../../../assets/data/myPageBoard.js"
 
-  export default {
-    data() {
-      return {
-        myNoticeInfo,
-        notificationLists
-      }
-    },
-    components: {
-      "basic-board-cast": basicBoardCast,
-      "notification-lists": notificationLists,
-      "sidebar-temporal": sidebarTemporal
+export default {
+  data() {
+    return {
+      myNoticeInfo,
+      notificationLists
     }
+  },
+  components: {
+    "basic-board-cast": basicBoardCast,
+    "notification-lists": notificationLists,
+    "sidebar-temporal": sidebarTemporal
   }
+}
 </script>
 <style scoped>
-  .notification-page {
-    width: 1200px;
-    display: flex;
-    margin: 0 auto;
-  }
-  .notification-page .sidebar {
-    display: inline-block;
-  }
-  .notification-page .basic-board-cast {
-    display: inline-block;
-    margin: 32px 0 0 16px;
-  }
+.notification-page {
+  width: 1200px;
+  display: flex;
+  margin: 0 auto;
+}
+.notification-page .sidebar {
+  display: inline-block;
+}
+.notification-page .basic-board-cast {
+  display: inline-block;
+  margin: 32px 0 0 16px;
+}
 </style>
--- pages/mypage/orderList/index.vue
+++ pages/mypage/orderList/index.vue
@@ -66,74 +66,74 @@
 </template>
 
 <script>
-  import { mapState, mapMutations } from "vuex"
-  import modal from "../../../components/modal/modal.vue"
-  import modalOrder from "../../../components/modal/modalOrder.vue"
-  import modalReceipt from "../../../components/modal/modalReceipt.vue"
-  import modalCancel from "../../../components/modal/modalCancel.vue"
-  import modalQna from "../../../components/modal/modalQna.vue"
-  import sidebarTemporal from "../../../components/myPage/sidebarTemporal.vue"
-  import basicBoardCast from "../../../components/common/basicBoardCast.vue"
-  import orderList from "../../../components/orderList/orderList.vue"
-  import { paymentInfo } from "../../../components/orderList/payment.js"
-  import { myOrderListInfo } from "../../../components/myPageBoard.js"
+import { mapState, mapMutations } from "vuex"
+import modal from "../../../components/modal/modal.vue"
+import modalOrder from "../../../components/modal/modalOrder.vue"
+import modalReceipt from "../../../components/modal/modalReceipt.vue"
+import modalCancel from "../../../components/modal/modalCancel.vue"
+import modalQna from "../../../components/modal/modalQna.vue"
+import sidebarTemporal from "../../../components/myPage/sidebarTemporal.vue"
+import basicBoardCast from "../../../components/common/basicBoardCast.vue"
+import orderList from "../../../components/orderList/orderList.vue"
+import { paymentInfo } from "../../../components/orderList/payment.js"
+import { myOrderListInfo } from "../../../assets/data/myPageBoard.js"
 
-  export default {
-    data() {
-      return {
-        paymentInfo,
-        myOrderListInfo,
-        modalIndex: 0
-      }
-    },
-    components: {
-      modal,
-      modalOrder,
-      modalReceipt,
-      modalCancel,
-      modalQna,
-      sidebarTemporal,
-      basicBoardCast,
-      orderList
-    },
-    computed: {
-      // store에서 각 데이터들 불러오는 부분
-      ...mapState("orderListModal", {
-        modalInfoCancel: "modalInfoCancel",
-        modalInfoOrder: "modalInfoOrder",
-        modalInfoReceipt: "modalInfoReceipt",
-        modalInfoQna: "modalInfoQna",
-        modalTypeOnOff: "modalTypeOpen"
-      }),
-      // 로컬 데이터를 limit값 기준으로 리스트 분할
-      computedListData() {
-        return this.paymentInfo.data.slice(
-          (this.paymentInfo.page - 1) * this.paymentInfo.limit,
-          this.paymentInfo.page * this.paymentInfo.limit
-        )
-      }
-    },
-    methods: {
-      modalId(id) {
-        this.modalIndex = id
-      },
-      ...mapMutations("orderListModal", ["modalOnOff"])
+export default {
+  data() {
+    return {
+      paymentInfo,
+      myOrderListInfo,
+      modalIndex: 0
     }
+  },
+  components: {
+    modal,
+    modalOrder,
+    modalReceipt,
+    modalCancel,
+    modalQna,
+    sidebarTemporal,
+    basicBoardCast,
+    orderList
+  },
+  computed: {
+    // store에서 각 데이터들 불러오는 부분
+    ...mapState("orderListModal", {
+      modalInfoCancel: "modalInfoCancel",
+      modalInfoOrder: "modalInfoOrder",
+      modalInfoReceipt: "modalInfoReceipt",
+      modalInfoQna: "modalInfoQna",
+      modalTypeOnOff: "modalTypeOpen"
+    }),
+    // 로컬 데이터를 limit값 기준으로 리스트 분할
+    computedListData() {
+      return this.paymentInfo.data.slice(
+        (this.paymentInfo.page - 1) * this.paymentInfo.limit,
+        this.paymentInfo.page * this.paymentInfo.limit
+      )
+    }
+  },
+  methods: {
+    modalId(id) {
+      this.modalIndex = id
+    },
+    ...mapMutations("orderListModal", ["modalOnOff"])
   }
+}
 </script>
 
 <style scoped>
-  .my-page-wrap {
-    width: 1200px;
-    display: flex;
-    margin: 0 auto;
-  }
-  .sidebar {
-    display: inline-block;
-    margin: 0;
-    padding: 0;
-  }
-  .basic-board-cast {
-    margin-top: 32px;
-  }
+.my-page-wrap {
+  width: 1200px;
+  display: flex;
+  margin: 0 auto;
+}
+.sidebar {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+}
+.basic-board-cast {
+  margin-top: 32px;
+}
 </style>
