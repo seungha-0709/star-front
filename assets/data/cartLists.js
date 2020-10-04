1b4be48ac986db31a01bb63e871be9199241a070
Seungha <sh.kim.x928@gmail.com>
2020-10-04 15:22:50 +0900

    Fix: cartList.js image key값 img로 수정 & 이미지 추가

---
+++ assets/data/cartLists.js
@@ -0,0 +1,24 @@
+export const cartLists = [
+  {
+    id: 96,
+    img:
+      "https://cdn.pixabay.com/photo/2015/05/31/11/32/work-791218_960_720.jpg",
+    title:
+      "상품명이 노출되는 영역. 상품명이 노출되는 영역. 상품명이 노출되는 영역입니다.",
+    amount: 1,
+    originPrice: 40000,
+    sale: 4000,
+    shippingFee: 0
+  },
+  {
+    id: 227,
+    img:
+      "https://cdn.pixabay.com/photo/2017/04/19/13/07/coffee-2242231_960_720.jpg",
+    title:
+      "상품명이 노출되는 영역. 상품명이 노출되는 영역. 상품명이 노출되는 영역입니다.",
+    amount: 1,
+    originPrice: 30000,
+    sale: 3000,
+    shippingFee: 2500
+  }
+]
