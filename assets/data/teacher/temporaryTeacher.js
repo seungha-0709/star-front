861698cbb3a963e9e26cfac92d96b91215780040
Seungha <sh.kim.x928@gmail.com>
2020-10-09 20:35:02 +0900

    Fix: 언론 속의 선생님 섹션 완성

--- assets/data/teacher/temporaryTeacher.js
+++ assets/data/teacher/temporaryTeacher.js
@@ -35,3 +35,22 @@ export const reviewLiveData = [
     goodbad: "good"
   }
 ]
+
+export const pressArticle = [
+  {
+    title: "1타 강사 오로라, 학원가를 강타하다",
+    site: "Naver"
+  },
+  {
+    title: "TOEIC RC 수강생 전원 혜택",
+    site: "Naver"
+  },
+  {
+    title: "토익 특강의 비밀",
+    site: "Daum"
+  },
+  {
+    title: "강사가 직접 말하는 학원의 진실",
+    site: "해커스어학원"
+  }
+]
--- components/teacher/teacherInPress.vue
+++ components/teacher/teacherInPress.vue
@@ -1,21 +1,60 @@
 <template>
   <div class="review-amount">
-    <header></header>
-    <main></main>
+    <header>
+        <h1>언론 속의 선생님</h1>
+        <img src="/img/teacher/rectangle@3x.png" alt="언론 속의 선생님" />
+    </header>
+    <main>
+        <ul>
+            <li v-for="(article, index) in pressArticle" :key="index">
+                {{ article.title }}
+                <span>{{ article.site }}</span>
+            </li>
+        </ul>
+    </main>
   </div>
 </template>
 
 <script>
-  export default {}
+import { pressArticle } from "../../assets/data/teacher/temporaryTeacher.js"
+  export default {
+      data() {
+          return {
+              pressArticle
+          }
+      }
+  }
 </script>
 
 <style scoped>
   .review-amount {
     width: 458px;
-    height: 212px;
     background: #fff;
     border: 1px solid #dfdfdf;
     padding: 32px;
     box-sizing: border-box;
+  }
+  h1 {
+      font-size: 20px;
+      color: #212121;
+      font-weight: bold;
+  }
+
+  header > img {
+      width: 394px;
+      display: block;
+      margin: 16px auto 12px auto;
+  }
+  li {
+      border-bottom: 1px solid #dfdfdf;
+      height: 42px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 14px;
+      color: #212121;
+  }
+  li > span {
+      display: inline-block
   }
 </style>
