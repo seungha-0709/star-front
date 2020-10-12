cc6db54acbf59730a5259fd1a44426f5e4025506
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-12 23:37:31 +0900

    Edit: join Term page

--- components/join/joinTerm.vue
+++ components/join/joinTerm.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="join-term-boxes">
     <div class="join-term-all"></div>
-    <div class="join-term-box"></div>
+    <div
+      class="join-term-box"
+      v-for="(item, index) in joinTermInfo"
+      :key="index"
+    >
+      <basic-join-term-box :title="item.title" :context="item.context" />
+    </div>
   </div>
 </template>
 <script>
-  import basicTermAll from "../common/join/basicTermAll.vue"
-  import basicTermBox from "../common/join/basicTermBox.vue"
+  import basicJoinTermAll from "../common/join/basicJoinTermAll.vue"
+  import basicJoinTermBox from "../common/join/basicJoinTermBox.vue"
+  import { joinTermInfo } from "../../assets/data/join/joinTerm.js"
 
   export default {
     data() {
-      return {}
+      return {
+        joinTermInfo
+      }
     },
     components: {
-      "basic-term-all": basicTermAll,
-      "basic-term-box": basicTermBox
+      "basic-term-all": basicJoinTermAll,
+      "basic-term-box": basicJoinTermBox
     }
   }
 </script>
