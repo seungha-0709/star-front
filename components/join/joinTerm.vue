4213bdb029aaca4ddaf4eb15a691d8c04dd79739
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-12 23:26:02 +0900

    Add: joinTerm component

---
+++ components/join/joinTerm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="join-term-boxes">
+    <div class="join-term-all"></div>
+    <div class="join-term-box"></div>
+  </div>
+</template>
+<script>
+  import basicTermAll from "../common/join/basicTermAll.vue"
+  import basicTermBox from "../common/join/basicTermBox.vue"
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      "basic-term-all": basicTermAll,
+      "basic-term-box": basicTermBox
+    }
+  }
+</script>
+<style scoped></style>
