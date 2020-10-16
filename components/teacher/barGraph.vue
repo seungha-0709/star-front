770fb91ae69de6fd3d9027aed0aab3a644545d1e
Seungha <sh.kim.x928@gmail.com>
2020-10-16 11:23:11 +0900

    Add: 리뷰 분포 bar그래프 추가

--- components/teacher/barGraph.vue
+++ components/teacher/barGraph.vue
@@ -1,12 +1,68 @@
 <template>
   <div class="bar-graph">
     <header></header>
-    <main></main>
+    <main>
+      <bar-graph-chart :chartData="datacollection" :options="options" />
+    </main>
   </div>
 </template>
 
+
+
+
 <script>
-  export default {}
+import barGraphChart from './barGraphChart.vue'
+
+export default {
+  components: {
+    barGraphChart
+  },
+  data() {
+      return {
+        datacollection: {},
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          scales: {
+            yAxes: [{
+              ticks: {
+                stepSize: 10,
+                min: 0,
+                max: 100
+            }}]
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            callbacks: {
+              label: function(tooltipItem) {
+                return tooltipItem.yLabel
+               }
+            }
+          }
+        }
+      }
+  },
+    mounted() {
+      this.fillData()
+    },
+    methods: {
+      fillData() {
+        this.datacollection = {
+          labels: ['긍정', '보통', '부정'],
+          datasets: [
+            {
+              label: '긍정',
+              backgroundColor: ['rgba(63, 96, 204, 0.5)', 'rgba(24, 173, 222, 0.4)', 'rgba(255, 51, 102, 0.5)'],
+              borderColor: ['#3f60cc', '#18adde', '#ff3366'],
+              data: [80, 40, 30]
+            }
+          ]
+        }
+      }
+      }
+    }
 </script>
 
 <style scoped>
