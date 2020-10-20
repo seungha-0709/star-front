00c52e8c1de8b6977605357acaf80ea7c79f5bc1
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-20 17:47:24 +0900

    Edit: add term agree function, change alert style

--- components/join/joinTerm.vue
+++ components/join/joinTerm.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="join-term-boxes">
     <div class="join-term-all">
-      <basic-join-term-all :term-all-agree="termAllAgree" />
+      <basic-join-term-all
+        :term-all-agree="termAllAgree"
+        @check-all="handleCheckAll"
+      />
     </div>
     <div
       class="join-term-box"
@@ -9,10 +12,17 @@
       :key="index"
     >
       <basic-join-term-box
-        :term-all-agree="termAllAgree"
+        :term-agree="termAllAgree[index]"
         :title="item.title"
         :context="item.context"
+        @check="handleCheck(index, termAllAgree[index])"
       />
+    </div>
+    <div class="alert" v-if="!termAllAgree[2]">
+      <p>
+        미동의 시 별별선생이 제공하는 다양한 학원/강사진의 학습 정보 전달이
+        제한될 수 있습니다.
+      </p>
     </div>
   </div>
 </template>
@@ -22,15 +32,26 @@
   import { joinTermInfo } from "../../assets/data/join/joinTerm.js"
 
   export default {
-    props: ["term-all-agree"],
     data() {
       return {
-        joinTermInfo
+        joinTermInfo,
+        termAllAgree: [false, false, false]
       }
     },
     components: {
       "basic-join-term-all": basicJoinTermAll,
       "basic-join-term-box": basicJoinTermBox
+    },
+    methods: {
+      handleCheckAll() {
+        const allCheckState = this.termAllAgree.every((item) => item === true)
+          ? [false, false, false]
+          : [true, true, true]
+        return (this.termAllAgree = allCheckState)
+      },
+      handleCheck(index, value) {
+        return this.termAllAgree.splice(index, 1, !value)
+      }
     }
   }
 </script>
@@ -38,5 +59,14 @@
   .join-term-boxes {
     width: 600px;
     margin: 0 auto;
+  }
+  .join-term-boxes .alert {
+    width: 600px;
+    font-size: 14px;
+    color: #ff3366;
+    margin: 16px auto 0;
+  }
+  .join-term-boxes .alert p {
+    text-align: left;
   }
 </style>
