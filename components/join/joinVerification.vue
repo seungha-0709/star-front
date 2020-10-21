e3607de597508a3fcef79829d757e8acfb024dbd
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-22 03:40:02 +0900

    Add: joinVerification components

---
+++ components/join/joinVerification.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="join-verification">
+    <div class="join-name"></div>
+    <div class="join-contact"></div>
+    <div class="resend-verification"></div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    components: {}
+  }
+</script>
+<style scoped>
+  .join-verification {
+    margin: 0 auto;
+    width: 600px;
+    height: 392px;
+    border: solid 1px #ececec;
+    background-color: #ffffff;
+    text-align: center;
+    vertical-align: middle;
+  }
+</style>
