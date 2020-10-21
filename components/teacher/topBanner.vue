9c336f503a8e4c9e7aedd0ab0b9f4013a6c54fb9
Seungha <sh.kim.x928@gmail.com>
2020-10-21 17:21:47 +0900

    Add: 강사 페이지 최상단 배너 영역 추가

---
+++ components/teacher/topBanner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="top-banner"></div>
+  </div>
+</template>
+
+<style scoped>
+  .top-banner {
+    width: 100%;
+    height: 140px;
+    background: #e9eef6;
+    margin-bottom: 32px;
+  }
+</style>
