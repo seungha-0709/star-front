17321bb773fa8225f65740988e90fd886c917807
dongmin <ehdals0227@gmail.com>
2020-10-23 17:13:51 +0900

    Add: lv0 index 페이지에 컴포넌트 일부 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      컨텐츠 내용
-      <div class="scroll-test">스크롤 영역</div>
-    </v-flex>
+    <banner-slide></banner-slide>
+    <move-site-search></move-site-search>
+    컨텐츠 내용
+    <div class="scroll-test">스크롤 영역</div>
   </v-layout>
 </template>
+
+<script>
+  import bannerSlide from "../components/levelZero/bannerSlide"
+  import moveSiteSearch from "../components/levelZero/moveSiteSearch"
+
+  export default {
+    components: {
+      "banner-slide": bannerSlide,
+      "move-site-search": moveSiteSearch
+    }
+  }
+</script>
 
 <style scoped>
   .scroll-test {
