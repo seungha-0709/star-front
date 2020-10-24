4e061df9f58b9950a73131ae954268c3e7b8626b
Seungha <sh.kim.x928@gmail.com>
2020-10-24 18:44:23 +0900

    Add: 강사 페이지 최상단 배너 추가

--- components/teacher/topBanner.vue
+++ components/teacher/topBanner.vue
@@ -8,7 +8,8 @@
   .top-banner {
     width: 100%;
     height: 140px;
-    background: #e9eef6;
+    background: url(/img/teacher/banner_top01@2x.png);
+    background-size: cover;
     margin-bottom: 32px;
   }
 </style>
