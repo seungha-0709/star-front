9a1f170320989a7f57378bddd3f1ea091dbdbe58
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-25 20:49:02 +0900

    Edit: add input value & event alert

--- components/join/joinVerification.vue
+++ components/join/joinVerification.vue
@@ -5,6 +5,7 @@
         name="이름"
         type="text"
         placeholder="인증을 위하여 본인의 실명을 입력해 주세요."
+        v-model="name"
       />
     </div>
     <div class="join-contact">
@@ -13,6 +14,7 @@
         width="367px"
         type="tel"
         placeholder="인증을 위해 휴대전화 번호를 입력해 주세요.(숫자만)"
+        v-model="phoneNumber"
       />
       <basic-button
         class="button"
@@ -23,6 +25,7 @@
         borderColor="#dfdfdf"
         borderRadius="0px"
         color="#212121"
+        @event="sendVerification"
       />
     </div>
     <div class="verification-confirm">
@@ -30,7 +33,11 @@
         width="367px"
         type="tel"
         placeholder="받으신 인증번호를 입력해 주세요."
+        v-model="inputCode"
       />
+      <div class="verification-time">
+        <p v-if="countSecond">03:21</p>
+      </div>
       <basic-button
         class="button"
         text="인증하기"
@@ -40,9 +47,10 @@
         borderColor="#3f60cc"
         borderRadius="0px"
         color="#3f60cc"
+        @event="confirmVerification"
       />
     </div>
-    <div class="resend">
+    <div v-on:click="sendVerification" class="resend">
       <p>인증번호 다시받기</p>
     </div>
   </div>
@@ -53,11 +61,31 @@
 
   export default {
     data() {
-      return {}
+      return {
+        verificationCode: "201025",
+        name: "",
+        phoneNumber: "",
+        inputCode: "",
+        countSecond: false
+      }
     },
     components: {
       "basic-verification-info": basicVerificationInfo,
       "basic-button": basicButton
+    },
+    methods: {
+      // eslint-disable-next-line space-before-function-paren
+      sendVerification: function (event) {
+        alert("인증번호는 " + this.verificationCode + "입니다.")
+        this.countSecond = true
+      },
+      // eslint-disable-next-line space-before-function-paren
+      confirmVerification: function (event) {
+        // eslint-disable-next-line eqeqeq
+        return this.inputCode === this.verificationCode
+          ? alert("인증이 완료되었습니다.")
+          : alert("인증번호를 다시 확인해주세요.")
+      }
     }
   }
 </script>
@@ -84,6 +112,15 @@
   }
   .join-verification .verification-confirm .button {
     margin-left: 16px;
+  }
+  .join-verification .verification-time {
+    position: absolute;
+    top: 562px;
+    left: 55%;
+  }
+  .join-verification .verification-time p {
+    color: #ff3366;
+    font-size: 14px;
   }
   .join-verification .resend {
     width: 107px;
