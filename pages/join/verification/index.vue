615fb66f595b5ba90e89b56a5f64a4c12035aade
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-26 00:35:59 +0900

    Edit: join-path 조건, joinInformation 컴포넌트 추가

--- pages/join/verification/index.vue
+++ pages/join/verification/index.vue
@@ -1,43 +1,48 @@
 <template>
-  <div class="join-verification-page">
-    <div class="join-verification-title">
-      <basic-join-title
-        title="회원가입"
-        subtitle="본인인증"
-        description="별별선생에 가입하시고"
-      />
-      <div class="join-title">
-        <p class="number">164,807</p>
-        <p>명 강사/학원 등에 대한 정보를 확인하세요.</p>
+  <div class="join-path">
+    <div v-if="(joinPath = 4)" class="join-verification-page">
+      <div class="join-verification-title">
+        <basic-join-title
+          title="회원가입"
+          subtitle="본인인증"
+          description="별별선생에 가입하시고"
+        />
+        <div class="join-title">
+          <p class="number">164,807</p>
+          <p>명 강사/학원 등에 대한 정보를 확인하세요.</p>
+        </div>
+      </div>
+      <div class="join-verification-group">
+        <join-verification />
+      </div>
+
+      <div class="banner">
+        <img src="/img/banner/invalid-name@2x.png" alt="배너이미지" />
+      </div>
+      <div class="button">
+        <basic-button
+          class="back-button"
+          text="이전"
+          width="160px"
+          height="51px"
+          borderRadius="31px"
+          background-color="rgba(0,0,0,0)"
+          fontWeight="normal"
+          color="#212121"
+          borderColor="#dfdfdf"
+        />
+        <basic-button
+          text="다음"
+          width="160px"
+          height="51px"
+          borderRadius="31px"
+          backgroundColor="#3f60cc"
+          fontWeight="normal"
+        />
       </div>
     </div>
-    <div class="join-verification-group">
-      <join-verification />
-    </div>
-
-    <div class="banner">
-      <img src="/img/banner/invalid-name@3x.png" alt="배너이미지" />
-    </div>
-    <div class="button">
-      <basic-button
-        class="back-button"
-        text="이전"
-        width="160px"
-        height="51px"
-        borderRadius="31px"
-        background-color="rgba(0,0,0,0)"
-        fontWeight="normal"
-        color="#212121"
-        borderColor="#dfdfdf"
-      />
-      <basic-button
-        text="다음"
-        width="160px"
-        height="51px"
-        borderRadius="31px"
-        backgroundColor="#3f60cc"
-        fontWeight="normal"
-      />
+    <div v-if="(joinPath = 5)" class="join-information-page">
+      <join-information />
     </div>
     <copy-right class="copyright" />
   </div>
@@ -45,22 +50,28 @@
 <script>
   import basicJoinTitle from "../../../components/common/join/basicJoinTitle.vue"
   import joinVerification from "../../../components/join/joinVerification.vue"
+  import joinInformation from "../../../components/join/joinInformation.vue"
   import basicButton from "../../../components/common/basicButton.vue"
   import copyright from "../../../components/common/copyright"
 
   export default {
     layout: "contentOnly",
-
+    data() {
+      return {
+        joinpath: 4
+      }
+    },
     components: {
       "basic-join-title": basicJoinTitle,
       "join-verification": joinVerification,
+      "join-information": joinInformation,
       "basic-button": basicButton,
       "copy-right": copyright
     }
   }
 </script>
 <style scoped>
-  .join-verification-page {
+  .join-path {
     margin: 0 auto;
     height: 100%;
     text-align: center;
