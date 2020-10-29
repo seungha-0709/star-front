e1ed810ed0afc6bb461df4707f9b44abf938c7a7
Hoyoung Kim <ghdud9606@gmail.com>
2020-10-29 12:51:09 +0900

    Edit: image > imageInfo 로 변경

--- assets/data/join/joinUser.js
+++ assets/data/join/joinUser.js
@@ -5,9 +5,9 @@ export const joinUserCardInfo = [
     description: "사용하는 이메일로 가입하기!",
     width: "107px",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/join_email.png",
-      alt: "email image",
+      alt: "이메일 로고",
       width: "120px",
       height: "120px"
     }
@@ -19,7 +19,7 @@ export const joinUserCardInfo = [
     backgroundColor: "#3ec728",
     width: "148px",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/join_naver.png",
       alt: "네이버 로고",
       width: "120px",
@@ -33,7 +33,7 @@ export const joinUserCardInfo = [
     backgroundColor: "#fee202",
     color: "#423630",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/join_kakao.png",
       alt: "카카오톡 로고",
       width: "120px",
@@ -46,7 +46,7 @@ export const joinUserCardInfo = [
     description: "사용하는 페이스북 계정으로 가입하기!",
     backgroundColor: "#3a5998",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/join_facebook.png",
       alt: "페이스북 로고",
       width: "120px",
@@ -61,7 +61,7 @@ export const joinSelectCardInfo = [
     type: "학생",
     description: "리뷰작성과 함께 모든 선생님들의 리뷰를 확인하실 수 있습니다.",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/student.png",
       alt: "학생 이미지",
       width: "120px",
@@ -74,7 +74,7 @@ export const joinSelectCardInfo = [
     description:
       "리뷰작성과 함께 우리 아이의 선생님과 유치원, 어린이집의 리뷰를 확인하실 수 있습니다.",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/parents.png",
       alt: "학부모 이미지",
       width: "120px",
@@ -87,7 +87,7 @@ export const joinSelectCardInfo = [
     description:
       "세상 가장 쉬운 방법으로 우리 학생들과 소통을 할 수 도, 내 강의를 등록할 수도 있습니다.",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/teacher.png",
       alt: "강사 이미지",
       width: "120px",
@@ -99,7 +99,7 @@ export const joinSelectCardInfo = [
     type: "학원/유치원 등 기업회원",
     description: "소속 강사, 학생 관리 서비스를 이용하실 수 있습니다.",
     link: "",
-    image: {
+    imageInfo: {
       src: "/img/image/academy.png",
       alt: "학원 이미지",
       width: "120px",
