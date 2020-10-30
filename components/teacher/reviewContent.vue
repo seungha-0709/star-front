817d158cf3e8d95ddea96f81a53c85677269c532
Seungha <sh.kim.x928@gmail.com>
2020-10-30 11:21:12 +0900

    Fix: basicbutton -> 클릭 안 되는 버튼은 div로 변경

--- components/teacher/reviewContent.vue
+++ components/teacher/reviewContent.vue
@@ -1,68 +1,72 @@
 <template>
   <div class="review-content">
     <header>
-    <ul>
-      <li v-for="(tab, index) in reviewContentTab" 
-        :key="index" 
-        :class="{ active: tab.type === activeTab }"
-        @click="onChangeActive(tab.type)">
-        {{ tab.name }}
-      </li>
-    </ul>
+      <ul>
+        <li
+          v-for="(tab, index) in reviewContentTab"
+          :key="index"
+          :class="{ active: tab.type === activeTab }"
+          @click="onChangeActive(tab.type)"
+        >
+          {{ tab.name }}
+        </li>
+      </ul>
     </header>
     <main>
       <section id="review" v-if="activeTab === 'review'">
         <article v-for="(review, index) in reviews" :key="index">
-          <basic-button v-if="review.tag === 'positive'"
-            text="긍정적인 리뷰"
-            borderColor="#5d7ee9"
-            backgroundColor="#ffffff"
-            color="#5d7ee9"
-            width="93px"
-            height="24px"
-            borderRadius="46.5px"
-            fontSize="12px" />
-          <basic-button v-if="review.tag === 'negative'"
-            text="부정적인 리뷰"
-            borderColor="#c1c1c1"
-            backgroundColor="#ffffff"
-            color="#666666"
-            width="93px"
-            height="24px"
-            borderRadius="46.5px"
-            fontSize="12px" />
-          <h2> "{{ review.title }}" </h2>
-          <div class="review-info">  
+          <div class="sticker-title positive" v-if="review.tag === 'positive'">
+            긍정적인 리뷰
+          </div>
+          <div class="sticker-title negative" v-if="review.tag === 'negative'">
+            부정적인 리뷰
+          </div>
+          <h2>"{{ review.title }}"</h2>
+          <div class="review-info">
             <div>
-              <div class="profile-image"></div>{{review.author}}
+              <div class="profile-image"></div>
+              {{ review.author }}
             </div>
-            <div>
-              {{review.category}}/{{review.level}}
-            </div>
+            <div>{{ review.category }}/{{ review.level }}</div>
           </div>
           <div class="review-text">
             <div>
-              <span><basic-button 
-                text="장점" 
-                width="55px" 
-                height="24px"
-                backgroundColor="#5d7ee9"
-                fontSize="12px" /></span>
-                {{ review.good }}
+              <span class="sticker-good">장점</span>
+              {{ review.good }}
             </div>
             <div>
-              <span><basic-button 
-                text="단점" 
-                width="55px" 
-                height="24px"
-                backgroundColor="#ff3366"
-                fontSize="12px" /></span>
-               {{ review.bad }}
-             </div>
+              <span class="sticker-bad">단점</span>
+              {{ review.bad }}
+            </div>
           </div>
           <div class="content-bottom">
             <div class="star-score">
-              6.0 별별별별별
+              <span class="score">{{ review.star.toFixed(1) }} </span>
+              <star-icon
+                class="star-icon"
+                size="1x"
+                :class="{ active: review.star >= 2 }"
+              />
+              <star-icon
+                class="star-icon"
+                size="1x"
+                :class="{ active: review.star >= 4 }"
+              />
+              <star-icon
+                class="star-icon"
+                size="1x"
+                :class="{ active: review.star >= 6 }"
+              />
+              <star-icon
+                class="star-icon"
+                size="1x"
+                :class="{ active: review.star >= 8 }"
+              />
+              <star-icon
+                class="star-icon"
+                size="1x"
+                :class="{ active: review.star >= 10 }"
+              />
             </div>
             <div class="button-wrap">좋아요, 답글</div>
           </div>
@@ -73,8 +77,7 @@
           <img src="/img/teacher/group-3@3x.png" alt="" />
           <div class="teacher-change">
             <div class="teacher-info-wrap">
-              <div class="teacher-image-small">
-              </div>
+              <div class="teacher-image-small"></div>
               <div class="teacher-info">
                 <strong>Before</strong><br />
                 오로로<br />
@@ -82,8 +85,7 @@
               </div>
             </div>
             <div class="teacher-info-wrap">
-              <div class="teacher-image-small">
-              </div>
+              <div class="teacher-image-small"></div>
               <div class="teacher-info">
                 <strong>After</strong><br />
                 오로라<br />
@@ -94,39 +96,25 @@
           <div class="transfer-feel">
             <div></div>
             <div>
-            <strong>Bad</strong><br />
-            다시 돌아갈 수 있다면<br /> 그렇게 하고 싶어요.
+              <strong>Bad</strong><br />
+              다시 돌아갈 수 있다면<br />
+              그렇게 하고 싶어요.
             </div>
           </div>
         </div>
         <article>
-          <h2>{{ transferReview.title }} </h2>
+          <h2>{{ transferReview.title }}</h2>
           <div>
-            <span><basic-button 
-              text="선생님을 왜 바꾸셨나요?" 
-              fontSize="12px"
-              width="155px" 
-              height="24px"
-              backgroundColor="#c1c1c1" /></span>
-              {{ transferReview.reason }}
+            <span>선생님을 왜 바꾸셨나요?</span>
+            {{ transferReview.reason }}
           </div>
           <div>
-              <span><basic-button 
-              text="before" 
-              fontSize="12px"
-              width="155px" 
-              height="24px"
-              backgroundColor="#c1c1c1" /></span>
-              {{ transferReview.before }}
+            <span>before</span>
+            {{ transferReview.before }}
           </div>
           <div>
-              <span><basic-button 
-              text="after" 
-              fontSize="12px"
-              width="155px" 
-              height="24px"
-              backgroundColor="#5d7ee9" /></span>
-              {{ transferReview.after }}
+            <span>after</span>
+            {{ transferReview.after }}
           </div>
         </article>
       </section>
@@ -139,7 +127,11 @@
 
 <script>
   import basicButton from "../../components/common/basicButton.vue"
-  import { reviews, transferReview } from "../../assets/data/teacher/temporaryTeacher.js"
+  import { StarIcon } from "vue-feather-icons"
+  import {
+    reviews,
+    transferReview
+  } from "../../assets/data/teacher/temporaryTeacher.js"
 
   export default {
     data() {
@@ -160,7 +152,8 @@
       }
     },
     components: {
-      basicButton
+      basicButton,
+      StarIcon
     },
     methods: {
       onChangeActive(type) {
@@ -210,7 +203,7 @@
   #review > article:nth-of-type(1) {
     border-right: 1px solid #dfdfdf;
   }
-  #review >article h2 {
+  #review > article h2 {
     color: #212121;
     font-weight: bold;
     font-size: 17px;
@@ -249,16 +242,16 @@
     overflow: hidden;
     font-size: 14px;
     color: #666;
-    /* 한 줄 자르기 */ 
-    display: inline-block; 
-    white-space: nowrap; 
-    text-overflow: ellipsis; 
-    /* 여러 줄 자르기 추가 스타일 */ 
-    white-space: normal; 
-    text-align: left; 
-    word-wrap: break-word; 
-    display: -webkit-box; 
-    -webkit-line-clamp: 5; 
+    /* 한 줄 자르기 */
+    display: inline-block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    /* 여러 줄 자르기 추가 스타일 */
+    white-space: normal;
+    text-align: left;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
     -webkit-box-orient: vertical;
   }
   .review-text > div:nth-child(1) {
@@ -278,7 +271,75 @@
     align-items: center;
   }
 
-/* 환승후기 */
+  /* 둥근 네모 */
+  .sticker-title.positive {
+    border: 1px solid #5d7ee9;
+    background: #ffffff;
+    color: #5d7ee9;
+    width: 93px;
+    height: 24px;
+    border-radius: 46.5px;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
+  .sticker-title.negative {
+    border: 1px solid #c1c1c1;
+    background: #ffffff;
+    color: #666;
+    width: 93px;
+    height: 24px;
+    border-radius: 46.5px;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
+  .sticker-good {
+    width: 55px;
+    height: 24px;
+    font-weight: bold;
+    color: #fff;
+    background: #5d7ee9;
+    display: block;
+    text-align: center;
+    border-radius: 16px;
+    line-height: 2;
+    font-size: 12px;
+  }
+  .sticker-bad {
+    width: 55px;
+    height: 24px;
+    font-weight: bold;
+    color: #fff;
+    background: #ff3366;
+    display: block;
+    text-align: center;
+    border-radius: 16px;
+    line-height: 2;
+    font-size: 12px;
+  }
+
+  /* 별 아이콘 */
+  .star-icon {
+    border: 0;
+    fill: currentColor;
+    color: #ececec;
+  }
+  .star-icon.active {
+    border: 0;
+    fill: currentColor;
+    color: #ffc13e;
+  }
+  span.score {
+    font-size: 28px;
+    font-weight: bold;
+  }
+
+  /* 환승후기 */
 
   #transfer {
     display: block;
@@ -380,20 +441,20 @@
     margin-bottom: 10px;
   }
   #transfer > article > div {
-    /* 한 줄 자르기 */ 
+    /* 한 줄 자르기 */
     font-size: 15px;
     color: #666;
     height: 85px;
     overflow: hidden;
-    display: inline-block; 
-    white-space: nowrap; 
-    text-overflow: ellipsis; 
-    /* 여러 줄 자르기 추가 스타일 */ 
-    white-space: normal; 
-    text-align: left; 
-    word-wrap: break-word; 
-    display: -webkit-box; 
-    -webkit-line-clamp: 3; 
+    display: inline-block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    /* 여러 줄 자르기 추가 스타일 */
+    white-space: normal;
+    text-align: left;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
   }
   footer {
