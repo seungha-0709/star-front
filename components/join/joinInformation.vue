46590ea46bd5caac46de4ffef6ace903f90a666b
Hoyoung Kim <ghdud9606@gmail.com>
2020-11-02 23:54:03 +0900

    Add: template codes & style

--- components/join/joinInformation.vue
+++ components/join/joinInformation.vue
@@ -19,6 +19,38 @@
         color="#212121"
       />
     </div>
+    <div class="join-password">
+      <join-verification-info
+        name="비밀번호"
+        type="password"
+        placeholder="비밀번호를 입력해주세요 (4~16자리)"
+        v-model="password"
+      />
+      <join-verification-info
+        type="password"
+        placeholder="비밀번호를 다시 한번 확인 입력해 주세요"
+        v-model="password"
+      />
+    </div>
+    <div class="join-nickname">
+      <join-verification-info
+        name="닉네임"
+        width="367px"
+        type="text"
+        placeholder="사용하실 닉네임을 입력해 주세요 (2~10자, 변경불가)"
+        v-model="nickname"
+      />
+      <basic-button
+        class="button"
+        text="중복확인"
+        width="137px"
+        background-color="#ffffff"
+        fontWeight="400"
+        borderColor="#dfdfdf"
+        borderRadius="0px"
+        color="#212121"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -43,7 +75,7 @@
   }
 </script>
 <style scoped>
-  .join-infomation {
+  .join-information {
     margin: 0 auto;
     padding: 40px;
     width: 600px;
@@ -53,10 +85,18 @@
     text-align: center;
     vertical-align: middle;
   }
-  .join-infomation .join-email {
+  .join-information .join-email {
     display: flex;
   }
-  .join-infomation .join-email .button {
+  .join-information .join-email .button,
+  .join-information .join-nickname .button {
     margin: 21px 0 0 16px;
+  }
+  .join-information .join-password {
+    margin-top: 16px;
+  }
+  .join-information .join-nickname {
+    display: flex;
+    margin-top: 32px;
   }
 </style>
