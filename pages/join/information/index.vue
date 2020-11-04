962b1c3624ee94f06118143e05665c7ce065d825
Hoyoung Kim <ghdud9606@gmail.com>
2020-11-05 03:20:11 +0900

    Edit: seperate 2 components

--- pages/join/information/index.vue
+++ pages/join/information/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="join-verification-page">
+  <div class="join-information-page">
     <basic-join-title
+      class="join-information-title"
       title="회원가입"
-      subtitle="본인인증"
-      description="별별선생에 가입하시고"
+      subtitle="기본정보"
+      description="별별선생에서 활동하실 아이디와 비밀번호, 닉네임 등의
+      기본정보를 생성합니다."
     />
-    <div class="join-verification-title">
-      <p class="title-number">164,807</p>
-      <p>명 강사/학원 등에 대한 정보를 확인하세요.</p>
-    </div>
-    <div class="join-verification">
-      <join-verification />
+    <div class="join-information">
+      <join-information />
     </div>
     <div class="banner">
       <img src="/img/banner/invalid-name@2x.png" alt="배너이미지" />
@@ -28,7 +26,7 @@
         borderColor="#dfdfdf"
       />
       <basic-button
-        text="다음"
+        text="가입완료"
         width="160px"
         height="51px"
         borderRadius="31px"
@@ -41,7 +39,7 @@
 </template>
 <script>
   import basicJoinTitle from "../../../components/common/join/basicJoinTitle.vue"
-  import joinVerification from "../../../components/join/joinVerification.vue"
+  import joinInformation from "../../../components/join/joinInformation.vue"
   import basicButton from "../../../components/common/basicButton.vue"
   import copyright from "../../../components/common/copyright"
 
@@ -50,47 +48,38 @@
 
     components: {
       "basic-join-title": basicJoinTitle,
-      "join-verification": joinVerification,
+      "join-information": joinInformation,
       "basic-button": basicButton,
       "copy-right": copyright
     }
   }
 </script>
 <style scoped>
-  .join-verification-page {
+  .join-information-page {
     margin: 0 auto;
     height: 100%;
     text-align: center;
     padding: 85px 0 90px;
   }
-  .join-verification-page .join-verification-title {
-    display: flex;
-    font-size: 20px;
-    color: #666666;
-    justify-content: center;
-    margin-top: -8px;
-  }
-
-  .join-verification-page .title-number {
-    font-weight: bold;
-    color: #5e3fcc;
-  }
-  .join-verification-page .join-verification {
+  .join-information-page .join-information {
     margin-top: 40px;
   }
-
-  .join-verification-page .banner {
+  .join-information-page .join-information-title {
+    width: 350px;
+    margin: 0 auto;
+  }
+  .join-information-page .banner {
     margin-top: 16px;
   }
-  .join-verification-page .banner img {
+  .join-information-page .banner img {
     width: 600px;
     height: 120px;
   }
-  .join-verification-page .button {
+  .join-information-page .button {
     text-align: center;
     margin: 32px 0 96px;
   }
-  .join-verification-page .button:first-child {
+  .join-information-page .button:first-child {
     margin-right: 16px;
   }
 </style>
