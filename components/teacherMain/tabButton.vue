5c7c4d3977df3df470f1b9d70f6569aa585641b8
Seungha <sh.kim.x928@gmail.com>
2020-11-04 13:50:59 +0900

    Add: lv1 메인페이지 탭 버튼 컴포넌트 추가

---
+++ components/teacherMain/tabButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div
+      class="tab-button"
+      :style="{
+        width: width,
+        height: height
+      }"
+      @click="$emit('tab-click')"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        default: ""
+      },
+      width: {
+        default: ""
+      },
+      height: {
+        default: ""
+      }
+    }
+    // data() {
+    //   return {
+    //     tabActive: false
+    //   }
+    // }
+    // methods: {
+    //   tabClick() {
+    //     this.tabActive = !this.tabActive
+    //   }
+    // }
+  }
+</script>
+<style scoped>
+  .tab-button.active {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+    font-weight: bold;
+    color: #3f60cc;
+    font-size: 16px;
+  }
+  .tab-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #eeeff4;
+    font-weight: normal;
+    color: #666;
+  }
+</style>
