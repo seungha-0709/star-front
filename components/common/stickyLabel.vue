c460fbeb1b2a965ba9575a7085fba3cb0542643c
Seungha <sh.kim.x928@gmail.com>
2020-11-04 22:46:50 +0900

    Add: stickyLabel 컴포넌트 추가

---
+++ components/common/stickyLabel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div
+      class="sticky-label"
+      :style="{
+        width,
+        height,
+        color,
+        backgroundColor,
+        borderRadius,
+        fontWeight,
+        fontSize
+      }"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        default: ""
+      },
+      width: {
+        default: ""
+      },
+      height: {
+        default: ""
+      },
+      color: {
+        default: "#fff"
+      },
+      backgroundColor: {
+        default: "#5d7ee9"
+      },
+      fontWeight: {
+        default: "bold"
+      },
+      borderRadius: {
+        default: "16px"
+      },
+      fontSize: {
+        default: "12px"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .sticky-label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
