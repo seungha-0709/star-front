d2a03db2d8574dc3b5b12e06dd432297a086c3d1
Seungha <sh.kim.x928@gmail.com>
2020-11-04 18:39:43 +0900

    Add: 별별정보 콘텐츠 리스트 추가

--- assets/data/teacherMain/infoBoard.js
+++ assets/data/teacherMain/infoBoard.js
@@ -1,5 +1,32 @@
 export const infoBoardNowList = [
     {
-        infoType: ""
+        infoType: "news",
+        title: "형사소송법 200제 문제 풀이 특강 상시 할인",
+        reply: 25
+    },
+    {
+        infoType: "info",
+        title: "법원직 9급 민법 150제 강의 리뉴얼 업데이트",
+        reply: 25
+    }, {
+        infoType: "tip",
+        title: "5월은 토목달, 해석 없이 푸는 파트 5, 6 꿀팁",
+        reply: 25
+    }, {
+        infoType: "tip",
+        title: "토익 리딩 시간 단축 꿀팁",
+        reply: 25
+    }, {
+        infoType: "tip",
+        title: "형사소송법 200제 문제 풀이 특강 상시 할인",
+        reply: 25
+    }, {
+        infoType: "news",
+        title: "형사소송법 200제 문제 풀이 특강 상시 할인",
+        reply: 25
+    }, {
+        infoType: "info",
+        title: "형사소송법 200제 문제 풀이 특강 상시 할인",
+        reply: 25
     }
 ]
--- components/teacherMain/infoBoard.vue
+++ components/teacherMain/infoBoard.vue
@@ -1,13 +1,91 @@
 <template>
-  <div class="background"></div>
+  <div class="background">
+    <ul>
+      <li v-for="(article, index) in infoBoardNowList" :key="index">
+        <div class="article-icons">
+          <mic-icon
+            size="1x"
+            class="article-icon"
+            v-if="(article.infoType === 'news')"
+          />
+          <check-circle-icon
+            size="1x"
+            class="article-icon"
+            v-if="(article.infoType === 'tip')"
+          />
+          <file-text-icon
+            size="1x"
+            class="article-icon"
+            v-if="(article.infoType === 'info')"
+          />
+        </div>
+        <span class="chat-board-title">{{ article.title }} </span>
+        <span class="reply">{{ `[${article.reply}]` }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script>
+  import { MicIcon, CheckCircleIcon, FileTextIcon } from "vue-feather-icons"
+  import { infoBoardNowList } from "../../assets/data/teacherMain/infoBoard.js"
+
+  export default {
+    data() {
+      return {
+        infoBoardNowList
+      }
+    },
+    components: {
+      MicIcon,
+      CheckCircleIcon,
+      FileTextIcon
+    }
+  }
+</script>
 <style scoped>
   .background {
-    width: 720px;
-    height: 297px;
-    border: 1px solid #dfdfdf;
-    background: #fff;
-    margin: 8px 4px 0px 0;
+    width: auto;
+    height: 212px;
+    overflow-y: scroll;
+    border: 0;
+  }
+  li {
+    margin-left: 32px;
+    margin-right: 32px;
+    width: auto;
+    height: 61px;
+    border-bottom: 1px solid #dfdfdf;
+    display: flex;
+    align-items: center;
+  }
+  li:hover {
+  }
+  li .chat-board-title {
+    font-size: 14px;
+    color: #212121;
+  }
+  li .reply {
+    font-size: 14px;
+    color: #ff3366;
+  }
+  li .reply::before {
+    content: "";
+    display: inline-block;
+    width: 8px;
+  }
+  .article-icons {
+    width: 36px;
+    height: 36px;
+    border-radius: 18px;
+    background: #f2f4f7;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 16px;
+  }
+  .article-icon {
+    color: #666;
+    opacity: 0.4;
   }
 </style>
