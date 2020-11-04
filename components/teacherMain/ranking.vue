7f93187624307ddf6f0c7075ede7ef94a9155ee8
Seungha <sh.kim.x928@gmail.com>
2020-11-04 13:51:33 +0900

    Fix: 탭 버튼 컴포넌트 클릭 이벤트 추가

--- components/teacherMain/ranking.vue
+++ components/teacherMain/ranking.vue
@@ -1,6 +1,52 @@
 <template>
-  <div class="background"></div>
+  <div class="background">
+    <div class="tab">
+      <tab-button
+        @tab-click="isTabActive(0)"
+        :tabState="tabState"
+        title="종합순위"
+        width="157px"
+        height="62px"
+      />
+      <tab-button
+        @tab-click="isTabActive(1)"
+        title="활동순위"
+        width="157px"
+        height="62px"
+      />
+      <tab-button
+        @tab-click="isTabActive(2)"
+        title="주요학원"
+        width="156px"
+        height="62px"
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+  import tabButton from "./tabButton.vue"
+  export default {
+    data() {
+      return {
+        tabState: true
+      }
+    },
+    components: {
+      tabButton
+    },
+    methods: {
+      isTabActive(index) {
+        const tabStates = document.getElementsByClassName("tab-button")
+        for (let i = 0; i < tabStates.length; i++)
+          tabStates[i].classList.remove("active")
+        document
+          .getElementsByClassName("tab-button")
+          [index].classList.add("active")
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .background {
@@ -9,5 +55,8 @@
     border: 1px solid #dfdfdf;
     background: #fff;
     margin: 4px 0px 0px 4px;
+  }
+  .tab {
+    display: flex;
   }
 </style>
