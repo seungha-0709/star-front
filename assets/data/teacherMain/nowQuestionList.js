c3eabd6f8214bea16905e97e1e5883692d82ac02
Seungha <sh.kim.x928@gmail.com>
2020-11-05 16:08:03 +0900

    Add: 별별질문 더미 데이터셋 추가

---
+++ assets/data/teacherMain/nowQuestionList.js
@@ -0,0 +1,57 @@
+export const nowQnaList = [
+    {
+        id: 1,
+        author: "고양이",
+        profileImage: "/img/peopleProfile/default_user01_04@2x.png",
+        title: "리뷰 승인 문의",
+        content: "지금 리뷰를 작성했는데, 언제 다른 리뷰를 볼 수 있나요? 원래 오래 걸리는 건지 궁금합니다. 내용은 CSS로 말줄임표 처리되도록 만들 것.",
+        depth: 1,
+        time: "10:11"
+    },
+    {
+        id: 2,
+        author: "별별운영자",
+        profileImage: "/img/peopleProfile/default_user01_06@2x.png",
+        title: "",
+        content: "별별선생의 이용은 작성해주신 리뷰가 심사가 완료된 시점부터 즉시 가능합니다. 고양이님께서 작성해주신 리뷰는 심사 완료 되었습니다. 이제부터 별별선생을 맘껏 이용하실 수 있습니다. :)",
+        depth: 2,
+        time: "10:32"
+    },
+    {
+        id: 3,
+        author: "냥냥펀치",
+        profileImage: "/img/peopleProfile/default_user01_07@2x.png",
+        title: "토익학원 얼마나 다녀야 할까요?",
+        content: "제목대로 문의드립니다. 이번에 토익학원을 첨다니려고 하는데, 대부분이 한두달까지 수업이더라구요. 그런걸로 성적이 오를 수 있을까요? 내용은 CSS로 말줄임표 처리되도록 만들 것",
+        depth: 1,
+        time: "9:24"
+    },
+    {
+        id: 4,
+        author: "상냥한별이양",
+        profileImage: "/img/peopleProfile/default_user02_06@2x.png",
+        title: "",
+        content: "안녕하세요. 별별선생입니다. 단기간에 성적향상이 이뤄지는 건 사실입니다만, 그런 만큼 한 번에 장시간의 수업이 이뤄지거나...",
+        depth: 2,
+        time: "9:24"
+    },
+    {
+        id: 5,
+        author: "아깽이냥냥",
+        profileImage: "/img/peopleProfile/default_user02_07@2x.png",
+        title: "리뷰 승인 언제 되나요?",
+        content: "3일 전에 리뷰를 작성했는데 아직까지도 리뷰 승인 대기라고 나와있네요. 언제쯤 리뷰가 승인되는 건가요?",
+        depth: 1,
+        time: "9:24"
+    },
+    {
+        id: 6,
+        author: "별별운영자",
+        profileImage: "/img/peopleProfile/default_user02_08@2x.png",
+        title: "",
+        content: "작성해주신 리뷰는 오늘 승인 완료되었습니다. 별별선생을 이용해주셔서 감사합니다.^-^ ",
+        depth: 2,
+        time: "9:24"
+    }
+    
+]
