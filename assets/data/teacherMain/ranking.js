b6d94efdb24534902577a73627ed43474e8044e1
Seungha <sh.kim.x928@gmail.com>
2020-11-05 16:09:29 +0900

    Add: 랭킹 컴포넌트 구현을 위한 더미 데이터셋 추가

---
+++ assets/data/teacherMain/ranking.js
@@ -0,0 +1,42 @@
+export const totalRankingList = [
+    {
+        ranking: 1,
+        name: "이윤상",
+        profileImage: "/img/teacherProfile/default_teacher02_07@2x.png",
+        star: 9.2,
+        reviews: 117,
+        institution: "가나다어학원"
+    },
+    {
+        ranking: 2,
+        name: "민신동",
+        profileImage: "/img/teacherProfile/default_teacher02_06@2x.png",
+        star: 8.5,
+        reviews: 98,
+        institution: "마바어학원"
+    },
+    {
+        ranking: 3,
+        name: "김영호",
+        profileImage: "/img/teacherProfile/default_teacher01_02@2x.png",
+        star: 8.1,
+        reviews: 84,
+        institution: "사아어학원"
+    },
+    {
+        ranking: 4,
+        name: "김하승",
+        profileImage: "/img/teacherProfile/default_teacher01_03@2x.png",
+        star: 7.6,
+        reviews: 67,
+        institution: "자차카어학원"
+    },
+    {
+        ranking: 5,
+        name: "이익재",
+        profileImage: "/img/teacherProfile/default_teacher02_04@2x.png",
+        star: 8.8,
+        reviews: 72,
+        institution: "타파하어학원"
+    }
+]
