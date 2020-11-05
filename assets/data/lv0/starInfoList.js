7fd61c372775dc34a8aa0ee19988681bc4eb8210
dongmin <ehdals0227@gmail.com>
2020-11-05 19:03:01 +0900

    Add: lv0 페이지에 필요한 데이터셋 추가 구성

---
+++ assets/data/lv0/starInfoList.js
@@ -0,0 +1,35 @@
+export const starInfoList = {
+  limit: 5,
+  data: [
+    {
+      thumbnail: "",
+      title: "별별수다 게시물 제목이 노출되는 영역입니다.",
+      level_name: "lv1명",
+      category: "꿀팁"
+    },
+    {
+      thumbnail: "",
+      title: "별별수다 게시물 제목이 노출되는 영역입니다.",
+      level_name: "lv1명",
+      category: "꿀팁"
+    },
+    {
+      thumbnail: "",
+      title: "별별수다 게시물 제목이 노출되는 영역입니다.",
+      level_name: "lv1명",
+      category: "꿀팁"
+    },
+    {
+      thumbnail: "",
+      title: "별별수다 게시물 제목이 노출되는 영역입니다.",
+      level_name: "lv1명",
+      category: "꿀팁"
+    },
+    {
+      thumbnail: "",
+      title: "별별수다 게시물 제목이 노출되는 영역입니다.",
+      level_name: "lv1명",
+      category: "꿀팁"
+    }
+  ]
+}
--- assets/data/lv0/starTalkList.js
+++ assets/data/lv0/starTalkList.js
@@ -63,6 +63,33 @@ export const starTalkList = {
       comment: 20,
       created_at: new Date(),
       title: "별별수다 게시물 제목이 노출되는 영역입니다."
+    },
+    {
+      member: {
+        thumbnail: "",
+        nickname: "작성자명"
+      },
+      comment: 20,
+      created_at: new Date(),
+      title: "별별수다 게시물 제목이 노출되는 영역입니다."
+    },
+    {
+      member: {
+        thumbnail: "",
+        nickname: "작성자명"
+      },
+      comment: 20,
+      created_at: new Date(),
+      title: "별별수다 게시물 제목이 노출되는 영역입니다."
+    },
+    {
+      member: {
+        thumbnail: "",
+        nickname: "작성자명"
+      },
+      comment: 20,
+      created_at: new Date(),
+      title: "별별수다 게시물 제목이 노출되는 영역입니다."
     }
   ]
 }
